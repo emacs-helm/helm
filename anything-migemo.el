e6e3d5aadd9c41181ab3e1aa8709cc19f9d1bf51
rubikitch <rubikitch>
2007-12-25 19:55:59 +0000

    patch is not needed anymore.

--- anything-migemo.el
+++ anything-migemo.el
@@ -1,5 +1,5 @@
 ;;; anything-migemo.el --- Migemo extension for anything
-;; $Id: anything-migemo.el,v 1.2 2007-12-25 13:05:46 rubikitch Exp $
+;; $Id: anything-migemo.el,v 1.3 2007-12-25 19:55:59 rubikitch Exp $
 
 ;; Copyright (C) 2007  rubikitch
 
@@ -29,11 +29,6 @@
 ;; `anything' is migemo-ized. This means that pattern matching of
 ;; `anything' candidates is done by migemo-expanded `anything-pattern'.
 
-;;; Preparation:
-
-;; Apply a patch here.
-;; http://www.rubyist.net/~rubikitch/archive/anything.el.migemo.patch
-
 ;;; Setting:
 
 ;; (require 'anything-config)
@@ -43,7 +38,10 @@
 ;;; History:
 
 ;; $Log: anything-migemo.el,v $
-;; Revision 1.2  2007-12-25 13:05:46  rubikitch
+;; Revision 1.3  2007-12-25 19:55:59  rubikitch
+;; patch is not needed anymore.
+;;
+;; Revision 1.2  2007/12/25 13:05:46  rubikitch
 ;; speed up by memoization
 ;;
 ;; Revision 1.1  2007/12/25 12:03:25  rubikitch
@@ -63,18 +61,11 @@ With prefix arugument, `anything-pattern' is migemo-ized, otherwise normal `anyt
 
 (defvar anything-previous-migemo-info '("" . "")
   "[Internal] Previous migemo query for anything-migemo.")
-(defun anything-string-match-with-migemo (re str)
+(defun anything-string-match-with-migemo (str)
   "Migemo version of `string-match'."
-  (unless (string= re (car anything-previous-migemo-info))
-    (setq anything-previous-migemo-info (cons re (migemo-get-pattern re))))
+  (unless (string= anything-pattern (car anything-previous-migemo-info))
+    (setq anything-previous-migemo-info (cons anything-pattern (migemo-get-pattern anything-pattern))))
   (string-match (cdr anything-previous-migemo-info) str))
-
-(defadvice anything-process-source (around anything-migemo activate)
-  "Anything search refinement using `anything-string-match-with-migemo' when `anything' is migemo-ized."
-  (if (not anything-use-migemo)
-      ad-do-it
-    (let ((anything-string-match-function 'anything-string-match-with-migemo))
-      ad-do-it)))
 
 (defadvice anything-get-sources (around anything-migemo activate)
   "All the `anything-sources' is delayed when `anything' is migemo-ized."
@@ -82,7 +73,9 @@ With prefix arugument, `anything-pattern' is migemo-ized, otherwise normal `anyt
       ad-do-it
     (setq ad-return-value
           (mapcar (lambda (source)
-                    (cons '(delayed) source))
+                    `((match . (anything-string-match-with-migemo))
+                      (delayed)
+                      ,@source))
                   ad-do-it))))
 
 
