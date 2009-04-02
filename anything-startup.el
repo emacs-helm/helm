3ac809e5e969fefc8a5075cad3196ea56f9fa97d
rubikitch <rubikitch>
2009-04-02 11:32:48 +0000

    *** empty log message ***

--- anything-startup.el
+++ anything-startup.el
@@ -1,27 +1,97 @@
 ;;; anything-startup.el --- anything.el startup file
 
-;;; Installation
+;;;; Installation
 
-;; anything.el and add-ons have complex dependencies. So you should
-;; install install-elisp.el to semi-automate Emacs Lisp installation.
-;; Install install-elisp.el from
-;; http://www.emacswiki.org/cgi-bin/wiki/download/install-elisp.el
-;; Then byte-compile and load it.
-(require 'install-elisp nil t)
-;; (setq install-elisp-repository-directory "~/.emacs.d/")
+;;; anything.el and add-ons have complex dependencies. So you should
+;;; install auto-install.el to semi-automate Emacs Lisp installation.
+;;; Install auto-install.el from
+;;; http://www.emacswiki.org/cgi-bin/wiki/download/auto-install.el
+;;; Then byte-compile and load it.
+(require 'auto-install nil t)
+(auto-install-compatibility-setup)
+(setq auto-install-directory "~/.emacs.d/auto-install/")
 
-;; anything.el is just a framework and predefined configuration is in
-;; anything-config.el. You need install both to use anything
-;; practically.
-
-;; [EVAL IT] (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/anything.el")
-;; [EVAL IT] (install-elisp "http://www.emacswiki.org/cgi-bin/wiki/download/anything-config.el")
-
-;; Note: anything-config.el loads anything.el.
+;;; anything.el is just a framework and predefined configuration is in
+;;; anything-config.el. You need install both to use anything
+;;; practically.
+;;; 
+;;; Note: anything-config.el loads anything.el.
+;;; 
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything.el")
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-config.el")
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-match-plugin.el")
+;;; 
+;;; anything-match-plugin.el extends pattern matching. Some Anything
+;;; Applications requires it. It is a must-have plugin now.
+;;; 
+(require 'anything-match-plugin)
 (require 'anything-config)
-(setq anything-idle-delay 0.5)
-(setq anything-input-idle-delay 0.2)
-(setq anything-sources
-      '(anything-c-source-buffers
-        anything-c-source-file-name-history
-        anything-c-source-locate))
+
+;;; If you use Japanese, you should install Migemo and anything-migemo.el.
+;;;
+;;; Migemo  http://0xcc.net/migemo/
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-migemo.el")
+(and (equal current-language-environment "Japanese")
+     (require 'anything-migemo nil t))
+
+;;; anything-complete.el replaces various completion with anything
+;;; (like Icicles). Use Anything power for normal completion.
+(require 'anything-complete nil t)
+
+;;; anything-auto-install.el integrates auto-install.el with anything.
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-auto-install")
+(require 'anything-auto-install nil t)
+
+;;; anything-yaoddmuse.el integrates yaoddmuse.el with anything.
+;;; [EVAL IT] (install-elisp-from-emacswiki "yaoddmuse.el")
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-yaoddmuse.el")
+(require 'anything-yaoddmuse nil t)
+
+;;; anything-emms.el integrates EMMS with anything.
+;;; EMMS  http://www.gnu.org/software/emms/
+;;; [EVAL IT] (install-elisp-from-emacswiki "emms-extension.el")
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-emms.el")
+(require 'anything-emms nil t)
+
+;;; anything-traverse.el integrates traverselisp.el with anything (like grep).
+;;; [EVAL IT] (install-elisp-from-emacswiki "traverselisp.el")
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-traverse.el")
+(require 'anything-traverse nil t)
+
+;;; anything-mercurial.el manages your mercurial patchs from anything (hg qpatch).
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-mercurial.el")
+(require 'anything-mercurial nil t)
+
+;;; anything-delicious.el manages your delicious bookmarks from anything.
+;;; (add, delete, update, browse, tag completion)
+;;;
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-delicious.el")
+(require 'anything-delicious nil t)
+
+;;; php-completion.el complets everything PHP with anything.
+;;; [EVAL IT] (install-elisp-from-emacswiki "php-completion.el")
+(require 'php-completion nil t)
+
+;;; anything-etags.el searches ExuberantCtags / EmacsTags tag.
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-etags.el")
+(require 'anything-etags nil t)
+
+;;; anything-gtags.el replaces gtags-select-mode with anything.
+;;; You have to install gtags.el.
+;;;
+;;; GNU GLOBAL  http://www.gnu.org/software/global/
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-gtags.el")
+(and (require 'gtags nil t)
+     (require 'anything-gtags nil t))
+
+;;; anything-include.el maintains history of #include and reusable (C, C++).
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-include.el")
+(require 'anything-include nil t)
+
+;;; descbinds-anything.el replaces describe-bindings with anything interface.
+;;; [EVAL IT] (install-elisp-from-emacswiki "descbinds-anything.el")
+(require 'descbinds-anything nil t)
+
+;;; anything-dabbrev-expand.el integrates dabbrev with anything.
+;;; [EVAL IT] (install-elisp-from-emacswiki "anything-dabbrev-expand.el")
+(require 'anything-dabbrev-expand nil t)
