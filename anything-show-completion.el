34d2f8f3cc2bd42a1a8c6435ea2a3803efe871cf
rubikitch <rubikitch>
2009-04-18 10:02:10 +0000

    doc

--- anything-show-completion.el
+++ anything-show-completion.el
@@ -1,5 +1,5 @@
 ;;; anything-show-completion.el --- Show selection in buffer for anything completion
-;; $Id: anything-show-completion.el,v 1.2 2009-04-18 09:21:14 rubikitch Exp $
+;; $Id: anything-show-completion.el,v 1.3 2009-04-18 10:02:10 rubikitch Exp $
 
 ;; Copyright (C) 2009  rubikitch
 
@@ -24,7 +24,30 @@
 
 ;;; Commentary:
 ;;
-;; 
+;; `anything' is also useful for in-buffer completion such as
+;; `anything-lisp-complete-symbol'. But users must see *anything*
+;; buffer when completing. It forces us to move our eyes away from
+;; code temporarily and we feel stressful.
+;;
+;; With this plug-in, current selection (`anything-get-selection') is
+;; displayed at point. This plug-in is automatically detected by user
+;; program such as anything-complete.el .
+
+;;; For developers:
+;;
+;; To enable anything-show-completion for user-defined function, use
+;; `use-anything-show-completion'. It accepts function and length of
+;; prefix (= current completing target) as a sexp. It must be used
+;; with soft-require.
+;;
+;; Example:
+;;   (when (require 'anything-show-completion nil t)
+;;     (use-anything-show-completion 'rct-complete-symbol--anything
+;;                                   '(length pattern)))
+;;
+;; Example in souce code:
+;;   http://www.emacswiki.org/cgi-bin/wiki/download/anything-complete.el
+;;   http://www.emacswiki.org/cgi-bin/wiki/download/anything-rcodetools.el
 
 ;;; Commands:
 ;;
@@ -35,6 +58,12 @@
 ;;
 ;; Below are customizable option list:
 ;;
+;;  `anything-show-completion-face'
+;;    *Face of anything-show-completion.
+;;    default = anything-selection-face
+;;  `anything-show-completion-activate'
+;;    *Set nil to turn off anything-show-completion.
+;;    default = t
 
 ;;; Installation:
 ;;
@@ -60,7 +89,10 @@
 ;;; History:
 
 ;; $Log: anything-show-completion.el,v $
-;; Revision 1.2  2009-04-18 09:21:14  rubikitch
+;; Revision 1.3  2009-04-18 10:02:10  rubikitch
+;; doc
+;;
+;; Revision 1.2  2009/04/18 09:21:14  rubikitch
 ;; `use-anything-show-completion' as a function.
 ;; It enables us to affect multiple commands with `dolist'.
 ;;
@@ -70,14 +102,21 @@
 
 ;;; Code:
 
-(defvar anything-show-completion-version "$Id: anything-show-completion.el,v 1.2 2009-04-18 09:21:14 rubikitch Exp $")
+(defvar anything-show-completion-version "$Id: anything-show-completion.el,v 1.3 2009-04-18 10:02:10 rubikitch Exp $")
 (require 'anything)
 (defgroup anything-show-completion nil
   "anything-show-completion"
   :group 'anything)
 
 (defvar asc-overlay nil)
-(defvar anything-show-completion-face anything-selection-face)
+(defcustom anything-show-completion-face anything-selection-face
+  "*Face of anything-show-completion."
+  :type 'face  
+  :group 'anything-show-completion)
+(defcustom anything-show-completion-activate t
+  "*Set nil to turn off anything-show-completion."
+  :type 'boolean  
+  :group 'anything-show-completion)
 
 (defun asc-initialize-maybe ()
   (unless asc-overlay
@@ -89,15 +128,9 @@
   (delete-overlay asc-overlay))
 (add-hook 'anything-cleanup-hook 'asc-cleanup)
 
-(defun use-anything-show-completion (f length-sexp)
-  (eval `(defadvice ,f (before anything-show-completion activate)
-           (asc-initialize-maybe)
-           (move-overlay asc-overlay (point) (point) (current-buffer))
-           (overlay-put asc-overlay 'length-sexp ',length-sexp))))
-
 (defun asc-overlay-activate-p ()
   "Return non-nil if `anything' is being used for any completionic purposes."
-  (overlay-buffer asc-overlay))
+  (and anything-show-completion-activate (overlay-buffer asc-overlay)))
 
 (defadvice anything-mark-current-line (after anything-show-completion activate)
   "Display the `anything-get-selection' contents as an overlay at the
@@ -115,6 +148,19 @@ current (point)."
                 (- (point) (eval (overlay-get asc-overlay 'length-sexp)))
                 (point)
                 anything-current-buffer))
+
+;;; Entry point
+(defun use-anything-show-completion (function prefix-length-sexp)
+  "Setup a before advice of FUNCTION to show the `anything-get-selection' contents as an overlay at point.
+
+PREFIX-LENGTH-SEXP is an expression to denote the length of prefix (completing target).
+It is evaluated in `asc-display-overlay'."
+  (eval `(defadvice ,function (before anything-show-completion activate)
+           (asc-initialize-maybe)
+           (move-overlay asc-overlay (point) (point) (current-buffer))
+           (overlay-put asc-overlay 'prefix-length-sexp ',prefix-length-sexp))))
+
+
 
 (provide 'anything-show-completion)
 
