c184e0cc47f3e8b7320676f14d86ba595e170296
thierry <thierry.volpiatto@gmail.com>
2010-01-01 14:19:24 +0100

    (anything-reduce-file-name): Fix for windows systems.

--- anything-config.el
+++ anything-config.el
@@ -1218,7 +1218,12 @@ If EXPAND is non--nil expand-file-name."
         (if (string= result "~/") "~/" result)
         (if (< level 0)
             (if empty "../" (concat "../" result))
-            (if empty "/" (concat "/" result))))))
+            (cond ((and (eq system-type 'windows-nt) empty)
+                   "C:/")
+                  ((and (not empty) (eq system-type 'windows-nt))
+                   result)
+                  ((not empty)
+                   (concat "/" result)))))))
 
 (defun anything-find-files-down-one-level (arg)
   "Go down one level like unix command `cd ..'.
