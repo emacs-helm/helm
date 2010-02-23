60cb47a35b0c6c639070f2653b7ad488516b0696
rubikitch <rubikitch>
2010-02-23 10:10:34 +0000

    implemented

--- anything-menu.el
+++ anything-menu.el
@@ -1,5 +1,5 @@
 ;;;; anything-menu.el --- menu command using anything interface
-;; $Id: anything-menu.el,v 1.1 2010-02-23 09:44:09 rubikitch Exp $
+;; $Id: anything-menu.el,v 1.2 2010-02-23 10:10:34 rubikitch Exp $
 
 ;; Copyright (C) 2010  rubikitch
 
@@ -60,19 +60,57 @@
 ;;; History:
 
 ;; $Log: anything-menu.el,v $
-;; Revision 1.1  2010-02-23 09:44:09  rubikitch
+;; Revision 1.2  2010-02-23 10:10:34  rubikitch
+;; implemented
+;;
+;; Revision 1.1  2010/02/23 09:44:09  rubikitch
 ;; initial
 ;;
 
 ;;; Code:
 
-(defvar anything-menu-version "$Id: anything-menu.el,v 1.1 2010-02-23 09:44:09 rubikitch Exp $")
-(eval-when-compile (require 'cl))
+(defvar anything-menu-version "$Id: anything-menu.el,v 1.2 2010-02-23 10:10:34 rubikitch Exp $")
+(require 'anything)
 (defgroup anything-menu nil
   "anything-menu"
   :group 'emacs)
 
+(defvar am/tmp-file "/tmp/.am-tmp-file")
+(defvar am/frame nil)
+(defun am/set-frame ()
+  (unless (and am/frame (frame-live-p am/frame))
+    (setq am/frame (make-frame '((name . "anything menu")
+                                 (title . "anything menu")))))
+  (select-frame am/frame)
+  (sit-for 0))
+
+(defun am/close-frame ()
+  (ignore-errors (make-frame-invisible am/frame))
+  (when (fboundp 'do-applescript)
+    (funcall 'do-applescript "tell application \"iTerm\"
+                                activate
+                             end")))
+(defun am/write-result (line)
+  (write-region (or line "") nil am/tmp-file))
+
+(defun anything-menu (&optional any-sources any-input any-prompt any-resume any-preselect any-buffer)
+  (interactive)
+  (am/set-frame)
+  (unwind-protect
+      (let ((anything-samewindow t)
+            (anything-display-function 'anything-default-display-buffer))
+        (anything any-sources any-input any-prompt any-resume any-preselect any-buffer))
+    (am/close-frame)))
+
+(defun anything-menu-select (am-prompt &rest am-selections)
+  (anything-menu `(((name . ,am-prompt)
+                    (candidates . am-selections)
+                    (action . am/write-result)))
+                 nil (concat am-prompt ": ") nil nil "*anything menu select*"))
 (provide 'anything-menu)
+
+;; (save-window-excursion (bg2 "gnudoit '(anything-menu-select \"selections\" \"a\" \"b\")'"))
+;; (find-sh0 "cat /tmp/.am-tmp-file")
 
 ;; How to save (DO NOT REMOVE!!)
 ;; (emacswiki-post "anything-menu.el")
