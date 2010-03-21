d0f93869a71b5bcac4638ce505b079ee1c337082
rubikitch <rubikitch>
2010-03-21 11:13:30 +0000

    `anything-grep' works asynchronously

--- anything-grep.el
+++ anything-grep.el
@@ -1,5 +1,5 @@
 ;;; anything-grep.el --- search refinement of grep result with anything
-;; $Id: anything-grep.el,v 1.25 2010-03-21 06:34:25 rubikitch Exp $
+;; $Id: anything-grep.el,v 1.26 2010-03-21 11:13:30 rubikitch Exp $
 
 ;; Copyright (C) 2008, 2009, 2010  rubikitch
 
@@ -62,7 +62,10 @@
 ;;; History:
 
 ;; $Log: anything-grep.el,v $
-;; Revision 1.25  2010-03-21 06:34:25  rubikitch
+;; Revision 1.26  2010-03-21 11:13:30  rubikitch
+;; `anything-grep' works asynchronously
+;;
+;; Revision 1.25  2010/03/21 06:34:25  rubikitch
 ;; New function: `anything-grep-by-name-reversed'
 ;;
 ;; Revision 1.24  2010/03/21 06:28:42  rubikitch
@@ -150,7 +153,7 @@
 
 ;;; Code:
 
-(defvar anything-grep-version "$Id: anything-grep.el,v 1.25 2010-03-21 06:34:25 rubikitch Exp $")
+(defvar anything-grep-version "$Id: anything-grep.el,v 1.26 2010-03-21 11:13:30 rubikitch Exp $")
 (require 'anything)
 (require 'grep)
 
@@ -210,31 +213,45 @@ The command is converting standard input to EUC-JP line by line. ")
 
 
 ;; (@* "core")
+(defvar anything-grep-sources nil
+  "`anything-sources' for last invoked `anything-grep'.")
 (defun anything-grep-base (sources)
   "Invoke `anything' for `anything-grep'."
   (and anything-grep-save-buffers-before-grep
        (save-some-buffers (not compilation-ask-about-save) nil))
-  (let ((anything-quit-if-no-candidate (lambda () (message "No matches"))))
+  (setq anything-grep-sources sources)
+  (let ((anything-quit-if-no-candidate t)
+        (anything-compile-source-functions
+         (cons 'anything-compile-source--agrep-init anything-compile-source-functions)))
     (anything sources nil nil nil nil "*anything grep*")))
 
 ;; (anything (list (agrep-source "grep -Hin agrep anything-grep.el" default-directory) (agrep-source "grep -Hin pwd anything-grep.el" default-directory)))
 
 (defun agrep-source (command pwd)
   "Anything Source of `anything-grep'."
-  (append
-   `((name . ,(format "%s [%s]" command pwd))
-     (command . ,command)
-     (pwd . ,pwd)
-     (init . agrep-init)
-     (candidates-in-buffer)
-     (action . agrep-goto)
-     (candidate-number-limit . 9999)
-     (migemo)
-     ;; to inherit faces
-     (get-line . buffer-substring))
-   (when anything-grep-multiline
-     '((multiline)
-       (real-to-display . agrep-real-to-display)))))
+  `((command . ,command)
+    (pwd . ,pwd)
+    (name . ,(format "%s [%s]" command pwd))
+    (anything-grep)))
+
+(defun anything-compile-source--agrep-init (source)
+  (if (assq 'anything-grep source)
+      (append '((init . agrep-init)) source)
+    source))
+
+(defun anything-compile-source--agrep-sentinel (source)
+  (if (assq 'anything-grep source)
+      `((action . agrep-goto)
+        (candidate-number-limit . 9999)
+        (migemo)
+        ;; to inherit faces
+        (candidates-in-buffer)
+        (get-line . buffer-substring)
+        ,@(when anything-grep-multiline
+            '((multiline)
+              (real-to-display . agrep-real-to-display)))
+        ,@source)
+    source))
 
 (defun agrep-init ()
   (agrep-create-buffer (anything-attr 'command)  (anything-attr 'pwd)))
@@ -247,6 +264,9 @@ The command is converting standard input to EUC-JP line by line. ")
               (substring file-line-content (match-end 0)))
     file-line-content))
 
+(defvar agrep-source-local nil)
+(defvar agrep-waiting-source nil
+  "`anything' sources to get together in `agrep-sentinel'.")
 (defun agrep-do-grep (command pwd)
   "Insert result of COMMAND. The current directory is PWD.
 GNU grep is expected for COMMAND. The grep result is colorized."
@@ -258,8 +278,24 @@ GNU grep is expected for COMMAND. The grep result is colorized."
       (setenv "GREP_COLOR" "01;31")
       ;; for GNU grep 2.5.1-cvs
       (setenv "GREP_COLORS" "mt=01;31:fn=:ln=:bn=:se=:ml=:cx=:ne"))
-    (call-process-shell-command (format "cd %s; %s" pwd command)
-                                nil (current-buffer))))
+    (set (make-local-variable 'agrep-source-local) (anything-get-current-source))
+    (add-to-list 'agrep-waiting-source agrep-source-local)
+    (set-process-sentinel
+     (start-process-shell-command "anything-grep" (current-buffer)
+                                  (format "cd %s; %s" pwd command))
+     'agrep-sentinel)))
+
+(defun agrep-sentinel (proc stat)
+  (with-current-buffer (process-buffer proc)
+    (setq agrep-waiting-source (delete agrep-source-local agrep-waiting-source))
+    (agrep-fontify))
+  (unless agrep-waiting-source
+    ;; call anything
+    (let ((anything-quit-if-no-candidate (lambda () (message "No matches")))
+          (anything-compile-source-functions
+           (cons 'anything-compile-source--agrep-sentinel anything-compile-source-functions)))
+      (anything anything-grep-sources nil nil nil nil "*anything grep*"))))
+
 (defun agrep-fontify ()
   "Fontify the result of `agrep-do-grep'."
   ;; Color matches.
@@ -286,7 +322,6 @@ Its contents is fontified grep result."
   (with-current-buffer (anything-candidate-buffer 'global)
     (setq default-directory pwd)
     (agrep-do-grep command pwd)
-    (agrep-fontify)
     (current-buffer)))
 ;; (display-buffer (agrep-create-buffer "grep --color=always -Hin agrep anything-grep.el" default-directory))
 ;; (anything '(((name . "test") (init . (lambda () (anything-candidate-buffer (get-buffer " *anything grep:grep --color=always -Hin agrep anything-grep.el*")) )) (candidates-in-buffer) (get-line . buffer-substring))))
