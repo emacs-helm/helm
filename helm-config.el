e6f32cf54cc551d258af86c708edd17688e828b7
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-03-22 18:58:56 +0100

    * helm-sys.el: new * helm-config.el: Move top, xrandr, emacs processes.

--- helm-config.el
+++ helm-config.el
@@ -47,6 +47,7 @@
 (require 'helm-man)
 (require 'helm-color)
 (require 'helm-font)
+(require 'helm-sys)
 (require 'helm-w3m nil t)
 (require 'helm-firefox nil t)
 (require 'helm-bmkext nil t)
@@ -1301,106 +1302,6 @@ See also `helm-create--actions'.")
                          (elscreen-goto (- (aref candidate 1) (aref "0" 0)))
                          (elscreen-kill-others)))))))
 
-
-;;;; <System>
-
-;;; Top (process)
-(defvar helm-c-top-command "COLUMNS=%s top -b -n 1"
-  "Top command (batch mode). %s is replaced with `frame-width'.")
-(defvar helm-c-source-top
-  '((name . "Top (Press C-c C-u to refresh)")
-    (init . helm-c-top-init)
-    (candidates-in-buffer)
-    (display-to-real . helm-c-top-display-to-real)
-    (persistent-action . helm-c-top-sh-persistent-action)
-    (persistent-help . "SIGTERM")
-    (action
-     ("kill (TERM)" . (lambda (pid)
-                        (helm-c-top-sh (format "kill -TERM %s" pid))))
-     ("kill (KILL)" . (lambda (pid)
-                        (helm-c-top-sh (format "kill -KILL %s" pid))))
-     ("Copy PID" . (lambda (pid) (kill-new pid))))))
-
-(defun helm-c-top-sh (cmd)
-  (message "Executed %s\n%s" cmd (shell-command-to-string cmd)))
-
-(defun helm-c-top-sh-persistent-action (pid)
-  (delete-other-windows)
-  (helm-c-top-sh (format "kill -TERM %s" pid))
-  (helm-force-update))
-
-(defun helm-c-top-init ()
-  (with-current-buffer (helm-candidate-buffer 'global)
-    (call-process-shell-command
-     (format helm-c-top-command
-             (- (frame-width) (if helm-enable-digit-shortcuts 4 0)))
-     nil (current-buffer))))
-
-(defun helm-c-top-display-to-real (line)
-  (car (split-string line)))
-
-;;; X RandR resolution change
-;;
-;;
-;;; FIXME I do not care multi-display.
-
-(defun helm-c-xrandr-info ()
-  "Return a pair with current X screen number and current X display name."
-  (with-temp-buffer
-    (call-process "xrandr" nil (current-buffer) nil
-                  "--current")
-    (let (screen output)
-      (goto-char (point-min))
-      (save-excursion
-        (when (re-search-forward "\\(^Screen \\)\\([0-9]\\):" nil t)
-          (setq screen (match-string 2))))
-      (when (re-search-forward "^\\(.*\\) connected" nil t)
-        (setq output (match-string 1)))
-      (list screen output))))
-
-(defun helm-c-xrandr-screen ()
-  "Return current X screen number."
-  (car (helm-c-xrandr-info)))
-
-(defun helm-c-xrandr-output ()
-  "Return current X display name."
-  (cadr (helm-c-xrandr-info)))
-
-(defvar helm-c-source-xrandr-change-resolution
-  '((name . "Change Resolution")
-    (candidates
-     . (lambda ()
-         (with-temp-buffer
-           (call-process "xrandr" nil (current-buffer) nil
-                         "--screen" (helm-c-xrandr-screen) "-q")
-           (goto-char 1)
-           (loop with modes = nil
-                 while (re-search-forward "   \\([0-9]+x[0-9]+\\)" nil t)
-                 for mode = (match-string 1)
-                 unless (member mode modes)
-                 collect mode into modes
-                 finally return modes))))
-    (action
-     ("Change Resolution"
-      . (lambda (mode)
-          (call-process "xrandr" nil nil nil
-                        "--screen" (helm-c-xrandr-screen)
-                        "--output" (helm-c-xrandr-output)
-                        "--mode" mode))))))
-
-;;; Emacs process
-;;
-;;
-(defvar helm-c-source-emacs-process
-  '((name . "Emacs Process")
-    (candidates . (lambda () (mapcar #'process-name (process-list))))
-    (persistent-action . (lambda (elm)
-                           (delete-process (get-process elm))
-                           (helm-delete-current-selection)))
-    (persistent-help . "Kill Process")
-    (action ("Kill Process" . (lambda (elm)
-                                (delete-process (get-process elm)))))))
-
 ;;; World time
 ;;
 ;;
@@ -1793,12 +1694,6 @@ http://cvs.savannah.gnu.org/viewvc/*checkout*/bm/bm/bm.el"
     (helm-other-buffer 'helm-c-source-bm "*helm bm list*")))
 
 ;;;###autoload
-(defun helm-list-emacs-process ()
-  "Preconfigured `helm' for emacs process."
-  (interactive)
-  (helm-other-buffer 'helm-c-source-emacs-process "*helm process*"))
-
-;;;###autoload
 (defun helm-browse-code ()
   "Preconfigured helm to browse code."
   (interactive)
@@ -1826,18 +1721,6 @@ See also `helm-create--actions'."
                     (action . (lambda (func) (funcall func string)))))))
 
 ;;;###autoload
-(defun helm-top ()
-  "Preconfigured `helm' for top command."
-  (interactive)
-  (let ((helm-samewindow t)
-        (helm-enable-shortcuts)
-        (helm-display-function 'helm-default-display-buffer)
-        (helm-candidate-number-limit 9999))
-    (save-window-excursion
-      (delete-other-windows)
-      (helm-other-buffer 'helm-c-source-top "*helm top*"))))
-
-;;;###autoload
 (defun helm-world-time ()
   "Preconfigured `helm' to show world time."
   (interactive)
@@ -1850,11 +1733,6 @@ See also `helm-create--actions'."
   (helm-other-buffer 'helm-c-source-ratpoison-commands
                      "*helm ratpoison commands*"))
 
-;;;###autoload
-(defun helm-xrandr-set ()
-  (interactive)
-  (helm :sources 'helm-c-source-xrandr-change-resolution
-        :buffer "*helm xrandr*"))
 
 
 ;;; Unit tests are now in ../developer-tools/unit-test-helm-config.el.
