56dfe4b54b46687f0329d5707e446c69f6b875d9
Jonas Bernoulli <jonas@bernoul.li>
2012-04-02 17:18:16 +0200

    helm*mode-line-string: improve readability of definitions

--- helm.el
+++ helm.el
@@ -350,10 +350,12 @@ If you prefer scrolling line by line, set this value to 1.")
 It is `helm-default-display-buffer' by default,
 which affects `helm-samewindow'.")
 
-(defvar helm-mode-line-string
-  "\\<helm-map>\\[helm-help]:Help \
+(defvar helm-mode-line-string "\
+\\<helm-map>\
+\\[helm-help]:Help \
 \\[helm-select-action]:Acts \
-\\[helm-exit-minibuffer]/\\[helm-select-2nd-action-or-end-of-line]/\
+\\[helm-exit-minibuffer]/\
+\\[helm-select-2nd-action-or-end-of-line]/\
 \\[helm-select-3rd-action]:NthAct \
 \\[helm-send-bug-report-from-helm]:BugReport"
   "Help string displayed in mode-line in `helm'.
