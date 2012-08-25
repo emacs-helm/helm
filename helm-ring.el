f75353fd3a18cc957bb536e5ebe8165c69002691
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-08-25 08:07:35 +0200

    * helm-ring.el (helm-c-source-mark-ring): Push mark when jumping to mark.

--- helm-ring.el
+++ helm-ring.el
@@ -157,7 +157,8 @@ replace with STR as yanked string."
                         it)))
     (action . (("Goto line"
                 . (lambda (candidate)
-                    (helm-goto-line (string-to-number candidate))))))
+                    (helm-goto-line (string-to-number candidate))
+                    (push-mark nil 'nomsg))))) 
     (persistent-action . (lambda (candidate)
                            (helm-goto-line (string-to-number candidate))
                            (helm-match-line-color-current-line)))
