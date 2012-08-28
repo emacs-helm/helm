f9ed9c27121ea5a22c097e763c20ff7e2948099f
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-08-28 07:45:26 +0200

    * helm-eshell.el (helm-c-source-eshell-history): Remove APPEND arg of eshell-write-history.  Thanks Akira.

--- helm-eshell.el
+++ helm-eshell.el
@@ -119,7 +119,7 @@ The function that call this should set `helm-ec-target' to thing at point."
     (init . (lambda ()
               (let (eshell-hist-ignoredups)
                 ;; Write the content's of ring to file.
-                (eshell-write-history eshell-history-file-name t)
+                (eshell-write-history eshell-history-file-name)
                 (with-current-buffer (helm-candidate-buffer 'global)
                   (insert-file-contents eshell-history-file-name)))
               ;; Same comment as in `helm-c-source-esh'
