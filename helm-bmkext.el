a20502aaf9d054ab33c969c5f11697bfabdb6cdd
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-09-23 08:48:00 +0200

    * helm-bmkext.el: Issue #120 declare bookmark-get-filename.

--- helm-bmkext.el
+++ helm-bmkext.el
@@ -21,6 +21,8 @@
 (require 'helm-bookmark)
 (require 'helm-adaptative)
 
+(declare-function bookmark-get-filename "bookmark" (bookmark-name-or-record))
+
 ;; Dependency: https://github.com/thierryvolpiatto/emacs-bmk-ext
 ;; If you want to enable google-maps in addressbook you will need
 ;; Julien Danjou google-maps-el package available here:
