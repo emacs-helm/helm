503a1750f5a7a281b57f42645b2911d172c5160f
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-11-25 17:35:51 +0100

    * helm-files.el (helm-c-source-find-files): Remove match function.

--- helm-files.el
+++ helm-files.el
@@ -363,7 +363,6 @@ Don't set it directly, use instead `helm-ff-auto-update-initial-value'.")
               (setq helm-ff-auto-update-flag
                     helm-ff-auto-update-initial-value)))
     (candidates . helm-find-files-get-candidates)
-    (match . helm-ff-match-function)
     (filtered-candidate-transformer helm-c-find-files-transformer)
     (persistent-action . helm-find-files-persistent-action)
     (persistent-help . "Hit1 Expand Candidate, Hit2 or (C-u) Find file")
@@ -410,9 +409,6 @@ Don't set it directly, use instead `helm-ff-auto-update-initial-value'.")
            ("View file" . view-file)
            ("Print File `C-c p, C-u to refresh'" . helm-ff-print)
            ("Locate `C-x C-f, C-u to specify locate db'" . helm-ff-locate))))))
-
-(defun helm-ff-match-function (candidate)
-  (string-match (regexp-quote helm-pattern) candidate))
 
 (defun helm-find-files-set-prompt-for-action (action files)
   "Set prompt for action ACTION for FILES."
