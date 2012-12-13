b24cfbb8da5d1a115435d9a63042a3b22d4ccfbf
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2012-12-13 07:38:53 +0100

    * helm-buffers.el: Issue #170 revert precedent changes. * helm-utils.el: put back helm-substring-by-width.

--- helm-utils.el
+++ helm-utils.el
@@ -213,6 +213,15 @@ all ITEM found in SEQ."
             else return index
             finally return ls))))
 
+(defun helm-substring-by-width (str width)
+  "Truncate string STR to end at column WIDTH.
+Like using `substring' from 0 to end, but handle multibyte characters."
+  (loop for ini-str = str
+        then (substring ini-str 0 (1- (length ini-str)))
+        for sw = (string-width ini-str)
+        when (<= sw width) return
+        (concat ini-str (make-string (- width sw) ? ))))
+
 (defun helm-c-get-pid-from-process-name (process-name)
   "Get pid from running process PROCESS-NAME."
   (loop with process-list = (list-system-processes)
