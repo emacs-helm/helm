c4cad2a2c86c24acc89ce73424b474fc782b9b32
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2013-01-24 07:07:11 +0100

    * helm-files.el: Issue #191 Remove ffap stuff.

--- helm-files.el
+++ helm-files.el
@@ -92,9 +92,7 @@
   :type  '(repeat (choice regexp)))
 
 (defcustom helm-for-files-preferred-list
-  '(helm-c-source-ffap-line
-    helm-c-source-ffap-guesser
-    helm-c-source-buffers-list
+  '(helm-c-source-buffers-list
     helm-c-source-recentf
     helm-c-source-bookmarks
     helm-c-source-file-cache
@@ -2459,61 +2457,6 @@ Else return ACTIONS unmodified."
     (mode-line . helm-generic-file-mode-line-string)
     (candidates . helm-c-file-cache-files)
     (type . file)))
-
-;;; ffap
-;;
-;;
-(eval-when-compile (require 'ffap))
-(defvar helm-c-source-ffap-guesser
-  `((name . "File at point")
-    (init . (lambda () (require 'ffap)))
-    (candidates . (lambda ()
-                    (helm-aif
-                        (with-helm-current-buffer
-                          (ffap-guesser))
-                        (list it))))
-    (keymap . ,helm-generic-files-map)
-    (help-message . helm-generic-file-help-message)
-    (mode-line . helm-generic-file-mode-line-string)
-    (action . ,(cdr (helm-get-attribute-from-type 'action 'file)))))
-
-;;; ffap with line number
-(defun helm-c-ffap-file-line-at-point ()
-  "Get (FILENAME . LINENO) at point."
-  (helm-aif (let (ffap-alist) (ffap-file-at-point))
-      (save-excursion
-        (forward-line 0)
-        (when (and (search-forward it nil t)
-                   (looking-at ":\\([0-9]+\\)"))
-          (cons it (string-to-number (match-string 1)))))))
-
-(defun helm-c-ffap-line-candidates ()
-  (with-helm-current-buffer
-    (helm-attrset 'ffap-line-location
-                  (helm-c-ffap-file-line-at-point)))
-  (helm-aif (helm-attr 'ffap-line-location)
-      (loop for (file . line) in (list it) collect
-            (cons (format "%s (line %d)" file line) file))))
-
-;;; Goto line after opening file by `helm-c-source-ffap-line'.
-(defun helm-c-ffap-line-goto-line ()
-  (helm-aif (helm-attr 'ffap-line-location)
-    (unwind-protect
-         (ignore-errors
-           (with-selected-window
-               (get-buffer-window
-                (get-file-buffer (car it)))
-             (helm-goto-line (cdr it))))
-      (helm-attrset 'ffap-line-location nil))))
-(add-hook 'helm-after-action-hook 'helm-c-ffap-line-goto-line)
-(add-hook 'helm-after-persistent-action-hook 'helm-c-ffap-line-goto-line)
-
-(defvar helm-c-source-ffap-line
-  `((name . "File/Lineno at point")
-    (init . (lambda () (require 'ffap)))
-    (candidates . helm-c-ffap-line-candidates)
-    (keymap . ,helm-map)
-    (action . ,(cdr (helm-get-attribute-from-type 'action 'file)))))
 
 
 ;;; Outliner
