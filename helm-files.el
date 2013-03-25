7dbf01285ffebf2c903b1a159adabf634d2545db
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2013-03-25 08:10:38 +0100

    * helm-files.el (helm-find-shell-command-fn): Fix case-fold-search.

--- helm-files.el
+++ helm-files.el
@@ -2543,25 +2543,27 @@ utility mdfind.")
 
 (defun helm-find-shell-command-fn ()
   "Asynchronously fetch candidates for `helm-find'."
-  (with-helm-default-directory (helm-default-directory)
-      (let (process-connection-type)
-        (prog1
-            (apply #'start-file-process "hfind" helm-buffer "find"
-                   (list "."
-                         (if case-fold-search "-name" "-iname")
-                         (concat "*" helm-pattern "*") "-type" "f"))
-          (set-process-sentinel
-           (get-process "hfind")
-           #'(lambda (process event)
-               (helm-process-deferred-sentinel-hook
-                process event (helm-default-directory))))))))
+  (let ((case-fold-search (helm-set-case-fold-search helm-pattern)))
+    (with-helm-default-directory (helm-default-directory)
+        (let (process-connection-type)
+          (prog1
+              (apply #'start-file-process "hfind" helm-buffer "find"
+                     (list "."
+                           (if case-fold-search "-iname" "-name")
+                           (concat "*" helm-pattern "*") "-type" "f"))
+            (set-process-sentinel
+             (get-process "hfind")
+             #'(lambda (process event)
+                 (helm-process-deferred-sentinel-hook
+                  process event (helm-default-directory)))))))))
 
 (defun helm-find-1 (dir)
   (helm :sources 'helm-source-findutils
         :buffer "*helm find*"
         ;; Make these vars local for further resuming.
         :default-directory dir ; reset it when called from elsewhere.
-        :ff-transformer-show-only-basename nil))
+        :ff-transformer-show-only-basename nil
+        :case-fold-search helm-file-name-case-fold-search))
 
 ;; helm-find-files integration.
 (defun helm-ff-find-sh-command (candidate)
