86571648131fa523176dbb04ee002f2e4c9bf975
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2013-04-09 09:05:14 +0200

    * helm-config.el: Font-lock define-helm-type-attribute.

--- helm-config.el
+++ helm-config.el
@@ -198,7 +198,8 @@
      ("(\\<\\(with-helm-show-completion\\)\\>" 1 font-lock-keyword-face)
      ("(\\<\\(with-helm-default-directory\\)\\>" 1 font-lock-keyword-face)
      ("(\\<\\(with-helm-display-same-window\\)\\>" 1 font-lock-keyword-face)
-     ("(\\<\\(with-helm-restore-variables\\)\\>" 1 font-lock-keyword-face))))
+     ("(\\<\\(with-helm-restore-variables\\)\\>" 1 font-lock-keyword-face)
+     ("(\\<\\(define-helm-type-attribute\\)\\>" 1 font-lock-keyword-face))))
 
 
 ;;; Start of automatically extracted autoloads.
