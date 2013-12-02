ebd5aaa05a48689f7b959ad0630a2c229c8be2d1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2013-12-02 13:21:20 +0100

    * helm-pkg.el: Update version.

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-pkg.el --- define helm for package.el -*- lexical-binding: t -*-
 
-(define-package "helm" "1.5.6"
+(define-package "helm" "1.5.7"
   "Helm is an Emacs incremental and narrowing framework")
 
 ;; Local Variables:
