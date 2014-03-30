67a46adc864c99bb9d299304dd501d2eed2aba20
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-03-30 09:55:56 +0200

    * helm-net.el (helm-wikipedia--summary-cache): Add caching. (helm-wikipedia--summary-cache): new hash-table.

--- helm-net.el
+++ helm-net.el
@@ -357,12 +357,18 @@ Return an alist with elements like (data . number_results)."
                                  (list (cons (format "Search for '%s' on wikipedia"
                                                      helm-pattern)
                                              helm-pattern)))))))
-      
+
+(defvar helm-wikipedia--summary-cache (make-hash-table :test 'equal))
 (defun helm-wikipedia-persistent-action (candidate)
   (message "Fetching summary from Wikipedia...")
-  (let (result)
+  (let (result mess)
     (while (progn
-             (setq result (helm-wikipedia-fetch-summary candidate))
+             (setq result (or (gethash candidate helm-wikipedia--summary-cache)
+                              (puthash candidate
+                                       (prog1
+                                           (helm-wikipedia-fetch-summary candidate)
+                                         (setq mess "Done"))
+                                       helm-wikipedia--summary-cache)))
              (when (and result
                         (listp result))
                (setq candidate (cdr result))
@@ -375,8 +381,9 @@ Return an alist with elements like (data . number_results)."
         (erase-buffer)
         (setq cursor-type nil)
         (insert result)
+        (fill-region (point-min) (point-max))
         (goto-char (point-min))
-        (message "Done."))))
+        (message mess))))
 
 
 (defun helm-wikipedia-fetch-summary (input)
