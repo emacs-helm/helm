b9f872f5ce230d71a5a80694983c9707ef0bc3e3
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-07-13 08:47:19 +0200

    * helm-files.el (helm-ff-query-replace-in-marked): New action. (helm-source-find-files): Add it in actions and reorder.

--- helm-files.el
+++ helm-files.el
@@ -417,14 +417,20 @@ Don't set it directly, use instead `helm-ff-auto-update-initial-value'.")
            ("Find file in Dired" . helm-point-file-in-dired)
            ,(and (locate-library "elscreen")
                  '("Find file in Elscreen"  . helm-elscreen-find-file))
+           ("View file" . view-file)
            ("Checksum File" . helm-ff-checksum)
+           ("Query replace in marked" . helm-ff-query-replace-in-marked)
+           ("Serial rename files" . helm-ff-serial-rename)
+           ("Serial rename by symlinking files" . helm-ff-serial-rename-by-symlink)
+           ("Serial rename by copying files" . helm-ff-serial-rename-by-copying)
+           ("Open file with default tool" . helm-open-file-with-default-tool)
+           ("Find file in hex dump" . hexl-find-file)
            ("Complete at point `C-c i'"
             . helm-insert-file-name-completion-at-point)
            ("Insert as org link `C-c @'" . helm-files-insert-as-org-link)
            ("Find shell command `C-c /'" . helm-ff-find-sh-command)
            ("Open file externally `C-c C-x, C-u to choose'"
             . helm-open-file-externally)
-           ("Open file with default tool" . helm-open-file-with-default-tool)
            ("Grep File(s) `C-s, C-u Recurse'" . helm-find-files-grep)
            ("Zgrep File(s) `M-g z, C-u Recurse'" . helm-ff-zgrep)
            ("Switch to Eshell `M-e'" . helm-ff-switch-to-eshell)
@@ -432,22 +438,17 @@ Don't set it directly, use instead `helm-ff-auto-update-initial-value'.")
            ("Eshell command on file(s) `M-!, C-u take all marked as arguments.'"
             . helm-find-files-eshell-command-on-file)
            ("Find file as root `C-x @'" . helm-find-file-as-root)
-           ("Find file in hex dump" . hexl-find-file)
            ("Ediff File `C-='" . helm-find-files-ediff-files)
            ("Ediff Merge File `C-c ='" . helm-find-files-ediff-merge-files)
            ("Delete File(s) `M-D'" . helm-delete-marked-files)
            ("Copy file(s) `M-C, C-u to follow'" . helm-find-files-copy)
            ("Rename file(s) `M-R, C-u to follow'" . helm-find-files-rename)
-           ("Serial rename files" . helm-ff-serial-rename)
-           ("Serial rename by symlinking files" . helm-ff-serial-rename-by-symlink)
-           ("Serial rename by copying files" . helm-ff-serial-rename-by-copying)
            ("Symlink files(s) `M-S, C-u to follow'" . helm-find-files-symlink)
            ("Relsymlink file(s) `C-u to follow'" . helm-find-files-relsymlink)
            ("Hardlink file(s) `M-H, C-u to follow'" . helm-find-files-hardlink)
            ("Find file other window `C-c o'" . find-file-other-window)
            ("Switch to history `M-p'" . helm-find-files-switch-to-hist)
            ("Find file other frame `C-c C-o'" . find-file-other-frame)
-           ("View file" . view-file)
            ("Print File `C-c p, C-u to refresh'" . helm-ff-print)
            ("Locate `C-x C-f, C-u to specify locate db'" . helm-ff-locate)))))
   "The main source to browse files.
@@ -829,6 +830,28 @@ Rename only file of current directory, and copy files coming from
 other directories.
 See `helm-ff-serial-rename-1'."
   (helm-ff-serial-rename-action 'copy))
+
+(defun helm-ff-query-replace-in-marked (_candidate)
+  (let ((marked (helm-marked-candidates))
+        (regexp (helm-read-string "Replace: "))
+        (str    (helm-read-string "With: ")))
+    (cl-loop with query = "y"
+             with count = 0
+             for old in marked
+             for new = (concat (helm-basedir old)
+                               (replace-regexp-in-string
+                                regexp str
+                                (helm-basename old)))
+             unless (string= old new)
+             do (progn
+                  (unless (string= query "!")
+                    (setq query (helm-read-string
+                                 (format "Replace `%s' by `%s' (!,y,n): "
+                                         old new))))
+                  (unless (string= query "n")
+                    (rename-file old new)
+                    (cl-incf count)))
+             finally (message "%d Files renamed" count))))
 
 (defun helm-ff-toggle-auto-update (_candidate)
   (setq helm-ff-auto-update-flag (not helm-ff-auto-update-flag))
