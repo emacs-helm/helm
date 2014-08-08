c7ebcaef15a4f6f961f275d38b4de29a954b594f
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-08-08 18:03:18 +0200

    * helm.el (helm-mark-all): Disable follow-mode if enabled (#590).

--- helm.el
+++ helm.el
@@ -4417,47 +4417,51 @@ Argument ACTION if present will be used as second argument of `display-buffer'."
   (interactive)
   (require 'helm-files)
   (with-helm-window
-    (let ((nomark (assq 'nomark (helm-get-current-source))))
-      (if nomark
-          (message "Marking not allowed in this source")
-        (save-excursion
-          (goto-char (helm-get-previous-header-pos))
-          (helm-next-line)
-          (let* ((next-head (helm-get-next-header-pos))
-                 (end       (and next-head
-                                 (save-excursion
-                                   (goto-char next-head)
-                                   (forward-line -1)
-                                   (point))))
-                 (maxpoint  (or end (point-max))))
-            (while (< (point) maxpoint)
-              (helm-mark-current-line)
-              (let* ((prefix (get-text-property (point-at-bol) 'display))
-                     (cand   (helm-get-selection))
-                     (bn     (and (helm-file-completion-source-p)
-                                  (helm-basename cand)))
-                     (src    (assoc-default 'name (helm-get-current-source))))
-                (when (and (not (helm-this-visible-mark))
-                           (not (or (string= prefix "[?]")
-                                    (string= prefix "[@]"))))
-                  ;; Don't mark possibles directories ending with . or ..
-                  ;; autosave files/links and non--existent file.
-                  (unless
-                      (and (or (helm-file-completion-source-p)
-                               (equal src "Files from Current Directory"))
-                           (or (string-match "^[.]?#.*#?$\\|[^#]*[.]\\{1,2\\}$" bn)
-                               ;; We need to test here when not using a transformer
-                               ;; that tag prefix (i.e on tramp)
-                               (not (file-exists-p cand))))
-                    (helm-make-visible-mark))))
-              (if (helm-pos-multiline-p)
-                  (progn
-                    (goto-char (or (helm-get-next-candidate-separator-pos) (point-max)))
-                    (forward-line 1))
-                (forward-line 1))
-              (end-of-line))))
-        (helm-mark-current-line)
-        (message "%s candidates marked" (length helm-marked-candidates))))))
+    (let ((nomark (assq 'nomark (helm-get-current-source)))
+          (follow (if helm-follow-mode 1 -1)))
+      (helm-follow-mode -1)
+      (unwind-protect
+           (if nomark
+               (message "Marking not allowed in this source")
+               (save-excursion
+                 (goto-char (helm-get-previous-header-pos))
+                 (helm-next-line)
+                 (let* ((next-head (helm-get-next-header-pos))
+                        (end       (and next-head
+                                        (save-excursion
+                                          (goto-char next-head)
+                                          (forward-line -1)
+                                          (point))))
+                        (maxpoint  (or end (point-max))))
+                   (while (< (point) maxpoint)
+                     (helm-mark-current-line)
+                     (let* ((prefix (get-text-property (point-at-bol) 'display))
+                            (cand   (helm-get-selection))
+                            (bn     (and (helm-file-completion-source-p)
+                                         (helm-basename cand)))
+                            (src    (assoc-default 'name (helm-get-current-source))))
+                       (when (and (not (helm-this-visible-mark))
+                                  (not (or (string= prefix "[?]")
+                                           (string= prefix "[@]"))))
+                         ;; Don't mark possibles directories ending with . or ..
+                         ;; autosave files/links and non--existent file.
+                         (unless
+                             (and (or (helm-file-completion-source-p)
+                                      (equal src "Files from Current Directory"))
+                                  (or (string-match "^[.]?#.*#?$\\|[^#]*[.]\\{1,2\\}$" bn)
+                                      ;; We need to test here when not using a transformer
+                                      ;; that tag prefix (i.e on tramp)
+                                      (not (file-exists-p cand))))
+                           (helm-make-visible-mark))))
+                     (if (helm-pos-multiline-p)
+                         (progn
+                           (goto-char (or (helm-get-next-candidate-separator-pos) (point-max)))
+                           (forward-line 1))
+                         (forward-line 1))
+                     (end-of-line))))
+               (helm-mark-current-line)
+               (message "%s candidates marked" (length helm-marked-candidates)))
+        (helm-follow-mode follow)))))
 
 (defun helm-unmark-all ()
   "Unmark all candidates in all sources of current helm session."
