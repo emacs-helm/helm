eb0eeba3af0fc160fa5fc15f3c653d640bd67a4e
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-08-24 12:42:13 +0200

    * helm-source.el (helm--make-source): Fix docstring.

--- helm-source.el
+++ helm-source.el
@@ -322,7 +322,7 @@ The function must return a process.")))
 Argument NAME is a string which define the source name, so no need to use
 the keyword :name in your source, NAME will be used instead.
 Argument CLASS is an eieio class object.
-Arguments ARGS are keyword value pairs as defined in `helm-source' which see."
+Arguments ARGS are keyword value pairs as defined in CLASS which see."
   (let ((source (apply #'make-instance class name args)))
     (oset source :name name)
     (helm--create-source source class)))
