b21ee98b00640e6a5ada4e31d7eea2b41f875453
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-11-11 11:40:31 +0100

    * helm.el (helm-search-match-part): Handle also fuzzy in multiples match.

--- helm.el
+++ helm.el
@@ -3991,12 +3991,18 @@ When using fuzzy matching, this function should be always called."
   (let ((part (funcall match-part-fn candidate))
         (fuzzy-p (assoc 'fuzzy-match (helm-get-current-source))))
     (if (string-match " " pattern)
-        ;; If pattern contain spaces assume
-        ;; we stop fuzzy matching and use multi pattern matching.
-        (cl-loop for i in (split-string pattern " " t)
-                 always (if (string-match "\\`!" i)
-                            (not (string-match (substring i 1) part))
-                            (string-match i part)))
+        (cl-loop for i in (split-string pattern " " t) always
+                 (if (string-match "\\`!" i)
+                     (not (string-match
+                           (if fuzzy-p
+                               (helm--mapconcat-candidate
+                                (substring i 1))
+                               (substring i 1))
+                           part))
+                     (string-match
+                      (if fuzzy-p
+                          (helm--mapconcat-candidate i) i)
+                      part)))
       (if (string-match "\\`!" pattern)
           (let ((reg (substring pattern 1)))
             (not (string-match (if fuzzy-p (helm--mapconcat-candidate reg) reg)
