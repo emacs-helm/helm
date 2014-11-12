1aa21f075b475545af4b2f12763873f994c1b156
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-11-12 18:45:11 +0100

    * emacs-helm.sh: Bind M-x to helm-M-x.

--- emacs-helm.sh
+++ emacs-helm.sh
@@ -85,6 +85,7 @@ cat > $TMP <<EOF
 (define-key global-map [remap occur] 'helm-occur)
 (define-key global-map [remap list-buffers] 'helm-buffers-list)
 (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (unless (boundp 'completion-in-region-function)
   (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
   (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
