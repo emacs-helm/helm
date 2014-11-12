35ab60d56c94c58fa15d34f36e0b5708635a20f1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2014-11-12 18:42:07 +0100

    Remove execute-extended-command from helm-completing-read-handlers-alist. * helm-mode.el (helm-completing-read-symbols): Revert to no history.

--- helm-mode.el
+++ helm-mode.el
@@ -31,7 +31,6 @@
     (describe-variable . helm-completing-read-symbols)
     (debug-on-entry . helm-completing-read-symbols)
     (find-function . helm-completing-read-symbols)
-    (execute-extended-command . helm-completing-read-symbols)
     (find-tag . helm-completing-read-with-cands-in-buffer)
     (ffap-alternate-file . nil)
     (tmm-menubar . nil))
@@ -488,40 +487,34 @@ that use `helm-comp-read' See `helm-M-x' for example."
     (prompt _collection test _require-match init
      hist default _inherit-input-method name buffer)
   "Specialized function for fast symbols completion in `helm-mode'."
-  (let ((sources
-         (list (helm-build-in-buffer-source name
-                 :init `(lambda ()
-                          (require 'helm-elisp)
-                          (with-current-buffer (helm-candidate-buffer 'global)
-                            (goto-char (point-min))
-                            (when (and ,default (stringp ,default)
-                                       ;; Some defaults args result as
-                                       ;; (symbol-name nil) == "nil".
-                                       ;; e.g debug-on-entry.
-                                       (not (string= ,default "nil"))
-                                       (not (string= ,default "")))
-                              (insert (concat ,default "\n")))
-                            (cl-loop for sym in (all-completions "" obarray ',test)
-                                     for s = (intern sym)
-                                     unless (or (and ,default (string= sym ,default))
-                                                (keywordp s))
-                                     do (insert (concat sym "\n")))))
-                 :persistent-action 'helm-lisp-completion-persistent-action
-                 :persistent-help "Show brief doc in mode-line")
-               (helm-build-sync-source (concat name " history")
-                 :candidates hist
-                 :persistent-action 'helm-lisp-completion-persistent-action
-                 :persistent-help "Show brief doc in mode-line"))))
-    (or
-     (helm
-      :sources (if helm-mode-reverse-history sources (reverse sources))
-      :prompt prompt
-      :buffer buffer
-      :input init
-      :history hist
-      :resume 'noresume
-      :default (or default ""))
-     (helm-mode--keyboard-quit))))
+  (or
+   (helm
+    :sources (helm-build-in-buffer-source name
+               :init `(lambda ()
+                        (require 'helm-elisp)
+                        (with-current-buffer (helm-candidate-buffer 'global)
+                          (goto-char (point-min))
+                          (when (and ,default (stringp ,default)
+                                     ;; Some defaults args result as
+                                     ;; (symbol-name nil) == "nil".
+                                     ;; e.g debug-on-entry.
+                                     (not (string= ,default "nil"))
+                                     (not (string= ,default "")))
+                            (insert (concat ,default "\n")))
+                          (cl-loop for sym in (all-completions "" obarray ',test)
+                                   for s = (intern sym)
+                                   unless (or (and ,default (string= sym ,default))
+                                              (keywordp s))
+                                   do (insert (concat sym "\n")))))
+               :persistent-action 'helm-lisp-completion-persistent-action
+               :persistent-help "Show brief doc in mode-line")
+    :prompt prompt
+    :buffer buffer
+    :input init
+    :history hist
+    :resume 'noresume
+    :default (or default ""))
+     (helm-mode--keyboard-quit)))
 
 
 ;;; Generic completing read
