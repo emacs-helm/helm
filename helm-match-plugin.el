6f356f0d4c40c5d64b00219b17e8ad3097434246
Michael Heerdegen <michael_heerdegen@web.de>
2014-12-16 13:19:21 +0100

    fix #746
    Use `add-face-text-property' instead of `put-text-property' to highlight
    matches so that faces in candidates are not just erased

--- helm-match-plugin.el
+++ helm-match-plugin.el
@@ -415,7 +415,7 @@ e.g \"bar foo\" will match \"barfoo\" but not \"foobar\" contrarily to
                       (< (point) end)
                       (< 0 (- (match-end 0) (match-beginning 0))))
             (unless (helm-pos-header-line-p)
-              (put-text-property (match-beginning 0) me 'face face)))
+              (add-face-text-property (match-beginning 0) me face)))
         (invalid-regexp nil)))))
 
 (defun helm-mp-highlight-match-internal (end)
