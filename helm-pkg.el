3d8ce2420822e954f2a8112e25ba4f2dba1d5647
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-02-09 14:27:26 +0100

    * helm-pkg.el: Update version, add url.

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,10 +1,11 @@
 ;;; helm-pkg.el --- define helm for package.el -*- lexical-binding: t -*-
 
-(define-package "helm" "1.6.6"
+(define-package "helm" "1.6.7"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
-    (async "1.2")))
+    (async "1.2"))
+  :url "http://github.com/emacs-helm/helm")
 
 ;; Local Variables:
 ;; byte-compile-warnings: (not cl-functions obsolete)
