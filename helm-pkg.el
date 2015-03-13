030796bf1696d9a74313b5b165285ea7681b1f9d
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-03-13 14:39:49 +0100

    * helm-pkg.el: Update version number, remove unneeded local vars.

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,6 +1,6 @@
-;;; helm-pkg.el --- define helm for package.el -*- lexical-binding: t -*-
+;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.6.7"
+(define-package "helm" "1.6.8"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
@@ -8,6 +8,5 @@
   :url "http://github.com/emacs-helm/helm")
 
 ;; Local Variables:
-;; byte-compile-warnings: (not cl-functions obsolete)
 ;; no-byte-compile: t
 ;; End:
