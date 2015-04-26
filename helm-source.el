103645a3a921a6d78e3bf728b9d3aa3de8b188d0
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-04-26 11:21:54 +0200

    * helm-source.el: Reindent.

--- helm-source.el
+++ helm-source.el
@@ -704,36 +704,36 @@ like `re-search-forward', see below documentation of :search slot.")
 (defmethod helm--setup-source :primary ((_source helm-type-file)))
 
 (defmethod helm--setup-source :before ((source helm-type-file))
-    (set-slot-value source 'action
-          (helm-make-actions
-           "Find file"                            'helm-find-many-files
-           "Find file as root"                    'helm-find-file-as-root
-           "Find file other window"               'find-file-other-window
-           "Find file other frame"                'find-file-other-frame
-           "Open dired in file's directory"       'helm-open-dired
-           "Grep File(s) `C-u recurse'"           'helm-find-files-grep
-           "Zgrep File(s) `C-u Recurse'"          'helm-ff-zgrep
-           "Pdfgrep File(s)"                      'helm-ff-pdfgrep
-           "Insert as org link"                   'helm-files-insert-as-org-link
-           "Checksum File"                        'helm-ff-checksum
-           "Ediff File"                           'helm-find-files-ediff-files
-           "Ediff Merge File"                     'helm-find-files-ediff-merge-files
-           "Etags `M-., C-u tap, C-u C-u reload tag file'"
-           'helm-ff-etags-select
-           "View file"                            'view-file
-           "Insert file"                          'insert-file
-           "Add marked files to file-cache"       'helm-ff-cache-add-file
-           "Delete file(s)"                       'helm-delete-marked-files
-           "Open file externally (C-u to choose)" 'helm-open-file-externally
-           "Open file with default tool"          'helm-open-file-with-default-tool
-           "Find file in hex dump"                'hexl-find-file))
-    (set-slot-value source 'persistent-help "Show this file")
-    (set-slot-value source 'action-transformer '(helm-transform-file-load-el
-                                       helm-transform-file-browse-url
-                                       helm-transform-file-cache))
-    (set-slot-value source 'candidate-transformer '(helm-skip-boring-files
-                                          helm-highlight-files
-                                          helm-w32-pathname-transformer)))
+  (set-slot-value source 'action
+                  (helm-make-actions
+                   "Find file"                            'helm-find-many-files
+                   "Find file as root"                    'helm-find-file-as-root
+                   "Find file other window"               'find-file-other-window
+                   "Find file other frame"                'find-file-other-frame
+                   "Open dired in file's directory"       'helm-open-dired
+                   "Grep File(s) `C-u recurse'"           'helm-find-files-grep
+                   "Zgrep File(s) `C-u Recurse'"          'helm-ff-zgrep
+                   "Pdfgrep File(s)"                      'helm-ff-pdfgrep
+                   "Insert as org link"                   'helm-files-insert-as-org-link
+                   "Checksum File"                        'helm-ff-checksum
+                   "Ediff File"                           'helm-find-files-ediff-files
+                   "Ediff Merge File"                     'helm-find-files-ediff-merge-files
+                   "Etags `M-., C-u tap, C-u C-u reload tag file'"
+                   'helm-ff-etags-select
+                   "View file"                            'view-file
+                   "Insert file"                          'insert-file
+                   "Add marked files to file-cache"       'helm-ff-cache-add-file
+                   "Delete file(s)"                       'helm-delete-marked-files
+                   "Open file externally (C-u to choose)" 'helm-open-file-externally
+                   "Open file with default tool"          'helm-open-file-with-default-tool
+                   "Find file in hex dump"                'hexl-find-file))
+  (set-slot-value source 'persistent-help "Show this file")
+  (set-slot-value source 'action-transformer '(helm-transform-file-load-el
+                                               helm-transform-file-browse-url
+                                               helm-transform-file-cache))
+  (set-slot-value source 'candidate-transformer '(helm-skip-boring-files
+                                                  helm-highlight-files
+                                                  helm-w32-pathname-transformer)))
 
 ;; Bookmarks
 (defclass helm-type-bookmark (helm-source) ()
