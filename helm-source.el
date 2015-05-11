5b2402084971bc1fd5019ac86524c852bb966662
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-05-11 08:24:05 +0200

    Add actions for files.
    * helm-source.el: Add actions to type files.
    * helm-locate.el: Bind these actions to generic map.

--- helm-source.el
+++ helm-source.el
@@ -712,27 +712,32 @@ like `re-search-forward', see below documentation of :search slot.")
   (set-slot-value
    source 'action
    (helm-make-actions
-    "Find file"                            'helm-find-many-files
-    "Find file as root"                    'helm-find-file-as-root
-    "Find file other window"               'find-file-other-window
-    "Find file other frame"                'find-file-other-frame
-    "Open dired in file's directory"       'helm-open-dired
-    "Grep File(s) `C-u recurse'"           'helm-find-files-grep
-    "Zgrep File(s) `C-u Recurse'"          'helm-ff-zgrep
-    "Pdfgrep File(s)"                      'helm-ff-pdfgrep
-    "Insert as org link"                   'helm-files-insert-as-org-link
-    "Checksum File"                        'helm-ff-checksum
-    "Ediff File"                           'helm-find-files-ediff-files
-    "Ediff Merge File"                     'helm-find-files-ediff-merge-files
+    "Find file"                             'helm-find-many-files
+    "Find file as root"                     'helm-find-file-as-root
+    "Find file other window"                'find-file-other-window
+    "Find file other frame"                 'find-file-other-frame
+    "Open dired in file's directory"        'helm-open-dired
+    "Grep File(s) `C-u recurse'"            'helm-find-files-grep
+    "Zgrep File(s) `C-u Recurse'"           'helm-ff-zgrep
+    "Pdfgrep File(s)"                       'helm-ff-pdfgrep
+    "Insert as org link"                    'helm-files-insert-as-org-link
+    "Checksum File"                         'helm-ff-checksum
+    "Ediff File"                            'helm-find-files-ediff-files
+    "Ediff Merge File"                      'helm-find-files-ediff-merge-files
     "Etags `M-., C-u tap, C-u C-u reload tag file'"
     'helm-ff-etags-select
-    "View file"                            'view-file
-    "Insert file"                          'insert-file
-    "Add marked files to file-cache"       'helm-ff-cache-add-file
-    "Delete file(s)"                       'helm-delete-marked-files
-    "Open file externally (C-u to choose)" 'helm-open-file-externally
-    "Open file with default tool"          'helm-open-file-with-default-tool
-    "Find file in hex dump"                'hexl-find-file))
+    "View file"                             'view-file
+    "Insert file"                           'insert-file
+    "Add marked files to file-cache"        'helm-ff-cache-add-file
+    "Delete file(s)"                        'helm-delete-marked-files
+    "Copy file(s) `M-C, C-u to follow'"     'helm-find-files-copy
+    "Rename file(s) `M-R, C-u to follow'"   'helm-find-files-rename
+    "Symlink files(s) `M-S, C-u to follow'" 'helm-find-files-symlink
+    "Relsymlink file(s) `C-u to follow'"    'helm-find-files-relsymlink
+    "Hardlink file(s) `M-H, C-u to follow'" 'helm-find-files-hardlink
+    "Open file externally (C-u to choose)"  'helm-open-file-externally
+    "Open file with default tool"           'helm-open-file-with-default-tool
+    "Find file in hex dump"                 'hexl-find-file))
   (set-slot-value source 'persistent-help "Show this file")
   (set-slot-value source 'action-transformer '(helm-transform-file-load-el
                                                helm-transform-file-browse-url
