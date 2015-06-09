77fe78684c8b0b4010d77c26371eff18ed3fc0ea
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-06-09 23:15:48 +0200

    Update version number.
    * helm-pkg.el: Do it.

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.7.1"
+(define-package "helm" "1.7.2"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
