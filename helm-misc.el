f8be88e86f769d5a5b434c954e4a8b1c5e5dcdca
Chunyang Xu <xuchunyang56@gmail.com>
2015-06-12 14:49:52 +0800

    Fix last commit
    Fix typo

--- helm-misc.el
+++ helm-misc.el
@@ -296,10 +296,10 @@ It is added to `extended-command-history'.
   (unless helm-source-buffers-list
     (setq helm-source-buffers-list
           (helm-make-source "Buffers" 'helm-source-buffers)))
-    (helm :sources helm-mini-default-sources
-          :buffer "*helm mini*"
-          :ff-transformer-show-only-basename nil
-          :truncate-lines helm-buffers-truncate-line))
+  (helm :sources helm-mini-default-sources
+        :buffer "*helm mini*"
+        :ff-transformer-show-only-basename nil
+        :truncate-lines helm-buffers-truncate-lines))
 
 ;;;###autoload
 (defun helm-minibuffer-history ()
