be9a5d3f4ebbcc7c6a8dea514529ffdc1ea2fd90
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-06-15 16:05:48 +0200

    Revert previous changes.
    * helm-regexp.el: Do it.

--- helm-regexp.el
+++ helm-regexp.el
@@ -223,11 +223,9 @@ arg METHOD can be one of buffer, buffer-other-window, buffer-other-frame."
   (let* ((split (helm-grep-split-line candidate))
          (buf (car split))
          (lineno (string-to-number (nth 1 split)))
-         (split-pat (helm-mp-split-pattern helm-input))
-         (switch-fn (if (get-buffer-window buf 'visible)
-                        #'pop-to-buffer #'switch-to-buffer)))
+         (split-pat (helm-mp-split-pattern helm-input)))
     (cl-case method
-      (buffer              (funcall switch-fn buf))
+      (buffer              (switch-to-buffer buf))
       (buffer-other-window (switch-to-buffer-other-window buf))
       (buffer-other-frame  (switch-to-buffer-other-frame buf)))
     (helm-goto-line lineno)
