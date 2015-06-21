d2ae9343953e2ab1b989e9560ea568624123d1c6
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-06-21 07:31:25 +0200

    Oops forget to update version in helm-pkg.el.
    * helm-pkg.el: Done.

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.7.2"
+(define-package "helm" "1.7.3"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
