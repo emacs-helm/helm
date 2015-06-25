a1dd4ffe2a4cad1fc0be399a0da45a51225440f5
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-06-25 13:56:31 +0200

    Remove unsused bindings in helm-find-files-map.
    * helm-files.el: Do it.

--- helm-files.el
+++ helm-files.el
@@ -346,9 +346,7 @@ I.e use the -path/ipath arguments of find instead of -name/iname."
     ;; Next 2 have no effect if candidate is not an image file.
     (define-key map (kbd "M-l")           'helm-ff-rotate-left-persistent)
     (define-key map (kbd "M-r")           'helm-ff-rotate-right-persistent)
-    (define-key map (kbd "C-.")           'helm-find-files-up-one-level)
     (define-key map (kbd "C-l")           'helm-find-files-up-one-level)
-    (define-key map (kbd "C-h C-b")       'helm-send-bug-report-from-helm)
     (define-key map (kbd "C-c r")         'helm-ff-run-find-file-as-root)
     (define-key map (kbd "C-c @")         'helm-ff-run-insert-org-link)
     (helm-define-key-with-subkeys map (kbd "DEL") ?\d 'helm-ff-delete-char-backward
