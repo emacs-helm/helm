e70bc6c4adc50d80ae18cfbab8424307e2f4c8f5
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-07-11 16:17:39 +0200

    * helm-core-pkg.el: Update description.

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,7 +1,7 @@
 ;;; helm-pkg.el --- define helm for package.el
 
 (define-package "helm-core" "1.7.4"
-  "Helm is an Emacs incremental and narrowing framework"
+  "Development files for Helm the Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
     (async "1.3"))
