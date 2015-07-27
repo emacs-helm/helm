0778a10081497f799f8762cb52be52132394b5f1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-07-27 09:18:21 +0200

    Quote preselection in generic function (read-file-name).
    * helm-mode.el (helm--generic-read-file-name): Quote preselection.
    (helm-read-file-name): Don't quote preselection here.

--- helm-mode.el
+++ helm-mode.el
@@ -857,8 +857,7 @@ Keys description:
                   :case-fold-search case-fold
                   :default default
                   :buffer buffer
-                  :preselect (and (stringp preselect)
-                                  (regexp-quote preselect)))))
+                  :preselect preselect)))
     (or
      (cond ((and result (stringp result)
                  (string= result "") ""))
@@ -966,6 +965,8 @@ Don't use it directly, use instead `helm-read-file-name' in your programs."
                        :default default-filename
                        :initial-input (expand-file-name init dir)
                        :alistp nil
+                       :preselect (and (stringp preselect)
+                                       (regexp-quote preselect))
                        :must-match mustmatch
                        :test predicate))))
       (helm-mode 1)
