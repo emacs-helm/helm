c302c0a1597324e454292e72946695caf5fa78b1
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-08-09 11:41:55 +0200

    Add messages in sexp and calculation (#1110)
    * helm-eval.el (helm-source-calculation-result,
    helm-build-evaluation-result-source): Do it.

--- helm-eval.el
+++ helm-eval.el
@@ -99,9 +99,11 @@ Should take one arg: the string to display."
     :action '(("Copy result to kill-ring" . (lambda (candidate)
                                               (kill-new
                                                (replace-regexp-in-string
-                                                "\n" "" candidate))))
+                                                "\n" "" candidate))
+                                              (message "Result copied to kill-ring")))
               ("copy sexp to kill-ring" . (lambda (_candidate)
-                                            (kill-new helm-input))))))
+                                            (kill-new helm-input)
+                                            (message "Sexp copied to kill-ring"))))))
 
 (defun helm-eval-new-line-and-indent ()
   (interactive)
@@ -149,7 +151,9 @@ Should take one arg: the string to display."
                                            (calc-eval helm-pattern)
                                          (error "error"))))
     :nohighlight t
-    :action '(("Copy result to kill-ring" . kill-new))))
+    :action '(("Copy result to kill-ring" . (lambda (candidate)
+                                              (kill-new candidate)
+                                              (message "Result copied to kill-ring"))))))
 
 ;;;###autoload
 (defun helm-eval-expression (arg)
