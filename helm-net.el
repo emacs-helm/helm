651530f98baf7f63db1da810c865ceebeb166301
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-09-12 09:18:57 +0200

    New browse url fn to detach firefox from emacs.
    * helm-net.el (helm-browse-url-firefox-new-window): New user var.
    (helm-browse-url-default-browser-alist): Add new firefox fn to default.
    (helm-browse-url-firefox): New.

--- helm-net.el
+++ helm-net.el
@@ -155,6 +155,14 @@ This is a format string, don't forget the `%s'."
   :group 'helm-net
   :type '(alist :key-type string :value-type function))
 
+(defcustom helm-browse-url-firefox-new-window "-new-tab"
+  "Allow choosing to browse url in new window or new tab.
+Can be \"-new-tab\" (default) or \"-new-window\"."
+  :group 'helm-net
+  :type '(radio
+          (const :tag "New tab" "-new-tab")
+          (const :tag "New window" "-new-window")))
+
 
 ;;; Additional actions for search suggestions
 ;;
@@ -371,7 +379,7 @@ This is a format string, don't forget the `%s'."
 (defvar helm-browse-url-default-browser-alist
   `((,(or (and (boundp 'w3m-command) w3m-command)
           "/usr/bin/w3m") . w3m-browse-url)
-    (,browse-url-firefox-program . browse-url-firefox)
+    (,browse-url-firefox-program . helm-browse-url-firefox)
     (,helm-browse-url-chromium-program . helm-browse-url-chromium)
     (,helm-browse-url-conkeror-program . helm-browse-url-conkeror)
     (,helm-browse-url-uzbl-program . helm-browse-url-uzbl)
@@ -396,6 +404,19 @@ This is a format string, don't forget the `%s'."
      #'(lambda (process event)
          (when (string= event "finished\n")
            (message "%s process %s" process event))))))
+
+(defun helm-browse-url-firefox (url &optional _ignore)
+  "Same as `browse-url-firefox' but detach from emacs.
+So when you quit emacs you can keep your firefox open
+and not be prompted to kill firefox process."
+  (interactive (list (read-string "URL: " (browse-url-url-at-point))
+                     nil))
+  (let ((process-environment (browse-url-process-environment)))
+    (call-process-shell-command
+     (format "(%s %s %s &)"
+             browse-url-firefox-program
+             helm-browse-url-firefox-new-window
+             (browse-url-encode-url url)))))
 
 (defun helm-browse-url-chromium (url &optional _ignore)
   "Browse URL with google chrome browser."
