fd375167853732c4b7ee716681732528f00b9aa8
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-09-23 18:59:02 +0200

    Update *pkg* files.

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,10 +1,10 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm-core" "1.7.8"
+(define-package "helm-core" "1.7.9"
   "Development files for Helm"
   '((emacs "24")
     (cl-lib "0.5")
-    (async "1.4"))
+    (async "1.5"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
--- helm-pkg.el
+++ helm-pkg.el
@@ -1,11 +1,11 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.7.8"
+(define-package "helm" "1.7.9"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24")
     (cl-lib "0.5")
-    (async "1.4")
-    (helm-core "1.7.8"))
+    (async "1.5")
+    (helm-core "1.7.9"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
