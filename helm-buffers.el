d4321e4ab31f7035bc8454fd7f99fbf1b48830c8
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-11-03 11:40:33 +0100

    Fix persistent action toggling for buffers (#1253).
    * helm-buffers.el (helm-buffers-list-persistent-action):
    Do it.

--- helm-buffers.el
+++ helm-buffers.el
@@ -756,11 +756,11 @@ If REGEXP-FLAG is given use `query-replace-regexp'."
 (defun helm-buffers-list-persistent-action (candidate)
   (if current-prefix-arg
       (helm-buffers-persistent-kill candidate)
-      (if (or (eql (window-buffer helm-persistent-action-display-window)
-                   (get-buffer helm-current-buffer))
-              (not (eq last-command 'helm-execute-persistent-action)))
-          (switch-to-buffer candidate)
-          (switch-to-buffer helm-current-buffer))))
+      (let ((current (window-buffer helm-persistent-action-display-window)))
+        (if (or (eql current (get-buffer helm-current-buffer))
+                (not (eql current (get-buffer candidate))))
+            (switch-to-buffer candidate)
+            (switch-to-buffer helm-current-buffer)))))
 
 (defun helm-ediff-marked-buffers (_candidate &optional merge)
   "Ediff 2 marked buffers or CANDIDATE and `helm-current-buffer'.
