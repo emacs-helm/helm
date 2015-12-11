490c37077f3154f2f2098aec05bf396c1b72d9c3
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2015-12-11 07:33:32 +0100

    Revert previous change as it is already available from C-c C-k (#1283).
    * helm-ring.el (helm-kill-ring-actions): Do it.

--- helm-ring.el
+++ helm-ring.el
@@ -52,8 +52,7 @@ If nil or zero (disabled), don't truncate candidate, show all."
     ("Delete" . (lambda (_candidate)
                   (cl-loop for cand in (helm-marked-candidates)
                            do (setq kill-ring
-                                    (delete cand kill-ring)))))
-    ("Kill new" . kill-new))
+                                    (delete cand kill-ring))))))
   "List of actions for kill ring source."
   :group 'helm-ring
   :type '(alist :key-type string :value-type function))
