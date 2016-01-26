1a7a2b5667196ad1c272c9d878c5ac4f9d9261d2
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-01-26 12:21:57 +0100

    New mode helm-popup-tip-mode (#1072).
    * helm-pkg.el: Add popup as dependency.
    * helm-utils.el (helm--show-help-echo-timer): New internal.
    (helm-sources-using-help-echo-popup): Same.
    (helm-cancel-help-echo-timer): New.
    (helm-show-help-echo): New.
    (helm-popup-tip-mode): New.

--- helm-pkg.el
+++ helm-pkg.el
@@ -4,6 +4,7 @@
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.3")
     (async "1.6")
+    (popup "0.5.3")
     (helm-core "1.9.1"))
   :url "https://emacs-helm.github.io/helm/")
 
--- helm-utils.el
+++ helm-utils.el
@@ -530,6 +530,49 @@ If STRING is non--nil return instead a space separated string."
 (add-hook 'helm-cleanup-hook 'helm-match-line-cleanup)
 (add-hook 'helm-after-persistent-action-hook 'helm-match-line-update)
 
+;;; Popup buffer-name or filename in grep/moccur/imenu-all.
+;;
+(defvar helm--show-help-echo-timer nil)
+(defvar helm-sources-using-help-echo-popup '("Moccur" "Imenu in all buffers"
+                                             "Ack-Grep" "AG" "Gid" "Git-Grep"))
+
+(defun helm-cancel-help-echo-timer ()
+  (when helm--show-help-echo-timer
+    (cancel-timer helm--show-help-echo-timer)
+    (setq helm--show-help-echo-timer nil)))
+
+(defun helm-show-help-echo ()
+  (when helm--show-help-echo-timer
+    (cancel-timer helm--show-help-echo-timer)
+    (setq helm--show-help-echo-timer nil))
+  (when (and helm-alive-p
+             (member (assoc-default 'name (helm-get-current-source))
+                     helm-sources-using-help-echo-popup))
+    (setq helm--show-help-echo-timer
+          (run-with-idle-timer
+           1 nil
+           (lambda ()
+             (with-helm-window
+               (helm-aif (get-text-property (point-at-bol) 'help-echo)
+                   (popup-tip (concat " " (abbreviate-file-name it))
+                              :around nil
+                              :point (save-excursion
+                                       (end-of-visual-line) (point))))))))))
+
+;;;###autoload
+(define-minor-mode helm-popup-tip-mode
+    "Show help-echo informations in a popup tip at end of line."
+  :global t
+  (cl-assert (featurep 'popup) nil "Popup package is not installed")
+  (if helm-popup-tip-mode
+      (progn
+        (add-hook 'helm-update-hook 'helm-show-help-echo) ; Needed for async sources.
+        (add-hook 'helm-move-selection-after-hook 'helm-show-help-echo)
+        (add-hook 'helm-cleanup-hook 'helm-cancel-help-echo-timer))
+      (remove-hook 'helm-update-hook 'helm-show-help-echo)
+      (remove-hook 'helm-move-selection-after-hook 'helm-show-help-echo)
+      (remove-hook 'helm-cleanup-hook 'helm-cancel-help-echo-timer)))
+
 (defun helm-open-file-with-default-tool (file)
   "Open FILE with the default tool on this platform."
   (let (process-connection-type)
