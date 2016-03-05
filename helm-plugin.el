1ded51d2c7561a4513c97f3a2871f81a29b25487
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-03-05 08:02:39 +0100

    Fix copyrights.

--- helm-plugin.el
+++ helm-plugin.el
@@ -1,6 +1,6 @@
 ;;; helm-plugin.el --- Helm plugins -*- lexical-binding: t -*-
 
-;; Copyright (C) 2012 ~ 2015 Thierry Volpiatto <thierry.volpiatto@gmail.com>
+;; Copyright (C) 2012 ~ 2016 Thierry Volpiatto <thierry.volpiatto@gmail.com>
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
