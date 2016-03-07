902ad0f051a3ba48178b0b33843d33d638fa6edb
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-03-07 10:41:18 +0100

    Fix version number in *.pkg files.

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,9 +1,9 @@
 ;;; helm-core-pkg.el --- define helm-core for package.el
 
-(define-package "helm-core" "1.9.2"
+(define-package "helm-core" "1.9.3"
   "Development files for Helm"
   '((emacs "24.3")
-    (async "1.6"))
+    (async "1.7"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
--- helm-pkg.el
+++ helm-pkg.el
@@ -1,11 +1,11 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.9.2"
+(define-package "helm" "1.9.3"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.3")
-    (async "1.6")
+    (async "1.7")
     (popup "0.5.3")
-    (helm-core "1.9.1"))
+    (helm-core "1.9.3"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
