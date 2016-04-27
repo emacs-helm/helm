40c0032f0d2bc91970830414efb64e5783d391df
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-04-27 18:01:34 +0200

    Version 1.9.5

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,9 +1,9 @@
 ;;; helm-core-pkg.el --- define helm-core for package.el
 
-(define-package "helm-core" "1.9.4"
+(define-package "helm-core" "1.9.5"
   "Development files for Helm"
   '((emacs "24.3")
-    (async "1.7"))
+    (async "1.8"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
--- helm-pkg.el
+++ helm-pkg.el
@@ -1,11 +1,11 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "1.9.4"
+(define-package "helm" "1.9.5"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.3")
-    (async "1.7")
+    (async "1.8")
     (popup "0.5.3")
-    (helm-core "1.9.4"))
+    (helm-core "1.9.5"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
