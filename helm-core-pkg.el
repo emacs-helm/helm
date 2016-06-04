07b5c098144e75005987050b6c3f3a52c0c09440
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-06-04 20:50:03 +0200

    Version 1.9.7

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-core-pkg.el --- define helm-core for package.el
 
-(define-package "helm-core" "1.9.6"
+(define-package "helm-core" "1.9.7"
   "Development files for Helm"
   '((emacs "24.3")
     (async "1.9"))
