emacs-helm/helm

commit 7c9223794c792f30f494e9941bf541cfbcea043e

Allow expanding directly to locate completion with / or C-j.
* helm-files.el (helm-ff-auto-expand-to-home-or-root):
Expand directly as soon as "/path/..foo/" is detected.
(helm-find-files-persistent-action): Just insert / in minibuffer
when "/path/..foo" is detected.

helm-files.el
@@ -1592,35 +1592,41 @@ or when `helm-pattern' is equal to \"~/\"."
 
 (defun helm-ff-auto-expand-to-home-or-root ()
   "Allow expanding to home/user directory or root or text yanked after pattern."
-  (when (and (helm-file-completion-source-p)
-             (string-match
-              "/?\\$.*/\\|/\\./\\|/\\.\\./\\|/~.*/\\|//\\|\\(/[[:alpha:]]:/\\|\\s\\+\\)"
-              helm-pattern)
-             (with-current-buffer (window-buffer (minibuffer-window)) (eolp))
-             (not (string-match helm-ff-url-regexp helm-pattern)))
-    (let* ((match (match-string 0 helm-pattern))
-           (input (cond ((string= match "/./")
-                         (expand-file-name default-directory))
-                        ((string= helm-pattern "/../") "/")
-                        ((string-match-p "\\`/\\$" match)
-                         (let ((sub (substitute-in-file-name match)))
-                           (if (file-directory-p sub)
-                               sub (replace-regexp-in-string "/\\'" "" sub))))
-                        (t (expand-file-name
-                            (helm-substitute-in-filename helm-pattern)
-                            ;; [Windows] On UNC paths "/" expand to current machine,
-                            ;; so use the root of current Drive. (i.e "C:/")
-                            (and (memq system-type '(windows-nt ms-dos))
-                                 (getenv "SystemDrive")) ; nil on Unix.
-                            )))))
-      (if (file-directory-p input)
-          (setq helm-ff-default-directory
-                (setq input (file-name-as-directory input)))
-        (setq helm-ff-default-directory (file-name-as-directory
-                                         (file-name-directory input))))
-      (with-helm-window
-        (helm-set-pattern input)
-        (helm-check-minibuffer-input)))))
+  (when (helm-file-completion-source-p)
+    (cond ((and (null (file-exists-p helm-pattern))
+                (string-match-p
+                 "\\`\\([.]\\|\\s-\\)\\{2\\}[^/]+"
+                 (helm-basename helm-pattern))
+                (string-match-p "/\\'" helm-pattern))
+           (helm-ff-recursive-dirs helm-pattern))
+          ((and (string-match
+                 "/?\\$.*/\\|/\\./\\|/\\.\\./\\|/~.*/\\|//\\|\\(/[[:alpha:]]:/\\|\\s\\+\\)"
+                 helm-pattern)
+                (with-current-buffer (window-buffer (minibuffer-window)) (eolp))
+                (not (string-match helm-ff-url-regexp helm-pattern)))
+           (let* ((match (match-string 0 helm-pattern))
+                  (input (cond ((string= match "/./")
+                                (expand-file-name default-directory))
+                               ((string= helm-pattern "/../") "/")
+                               ((string-match-p "\\`/\\$" match)
+                                (let ((sub (substitute-in-file-name match)))
+                                  (if (file-directory-p sub)
+                                      sub (replace-regexp-in-string "/\\'" "" sub))))
+                               (t (expand-file-name
+                                   (helm-substitute-in-filename helm-pattern)
+                                   ;; [Windows] On UNC paths "/" expand to current machine,
+                                   ;; so use the root of current Drive. (i.e "C:/")
+                                   (and (memq system-type '(windows-nt ms-dos))
+                                        (getenv "SystemDrive")) ; nil on Unix.
+                                   )))))
+             (if (file-directory-p input)
+                 (setq helm-ff-default-directory
+                       (setq input (file-name-as-directory input)))
+                 (setq helm-ff-default-directory (file-name-as-directory
+                                                  (file-name-directory input))))
+             (with-helm-window
+               (helm-set-pattern input)))))
+    (helm-check-minibuffer-input)))
 
 (defun helm-substitute-in-filename (fname)
   "Substitute all parts of FNAME from start up to \"~/\" or \"/\".
@@ -2412,8 +2418,9 @@ If a prefix arg is given or `helm-follow-mode' is on open file."
           ;; Start a recursive search for directories.
           ((and (not (file-exists-p candidate))
                 (not (file-remote-p candidate))
-                (string-match-p "/\\'" candidate))
-           (helm-ff-recursive-dirs candidate))
+                (string-match-p "\\`\\([.]\\|\\s-\\)\\{2\\}[^/]+"
+                                (helm-basename candidate)))
+           (funcall insert-in-minibuffer (concat candidate "/")))
           ;; On second hit we open file.
           ;; On Third hit we kill it's buffer maybe.
           (t

commit 98ff992d7dcaecdc0a072de14d07da1a0c3296f1

Run helm-check-minibuffer-input within helm window
Otherwise I get one quick refresh every time after I call
`helm-find-files'.

The issue was introduced in 7c92237

Updated with patch from Thierry

helm-files.el
@@ -1598,7 +1598,8 @@ or when `helm-pattern' is equal to \"~/\"."
                  "\\`\\([.]\\|\\s-\\)\\{2\\}[^/]+"
                  (helm-basename helm-pattern))
                 (string-match-p "/\\'" helm-pattern))
-           (helm-ff-recursive-dirs helm-pattern))
+           (helm-ff-recursive-dirs helm-pattern)
+           (with-helm-window (helm-check-minibuffer-input)))
           ((and (string-match
                  "/?\\$.*/\\|/\\./\\|/\\.\\./\\|/~.*/\\|//\\|\\(/[[:alpha:]]:/\\|\\s\\+\\)"
                  helm-pattern)
@@ -1625,8 +1626,8 @@ or when `helm-pattern' is equal to \"~/\"."
                  (setq helm-ff-default-directory (file-name-as-directory
                                                   (file-name-directory input))))
              (with-helm-window
-               (helm-set-pattern input)))))
-    (helm-check-minibuffer-input)))
+               (helm-set-pattern input)
+               (helm-check-minibuffer-input)))))))
 
 (defun helm-substitute-in-filename (fname)
   "Substitute all parts of FNAME from start up to \"~/\" or \"/\".