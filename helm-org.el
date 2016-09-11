dfc86f9beecee5ca93dd0ca4d63bdd89e41908a2
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-09-11 23:11:13 +0200

    Fix commands and bindings in helm-org (#1602).
    * helm-org.el (helm-org-headings-actions): Fix action tags.
    (helm-org-run-open-heading-in-indirect-buffer): New.
    (helm-org-headings-map): Use new commands.
    (helm-org-run-insert-link-to-heading-at-marker): New
    (helm-org-run-heading-refile): New.

--- helm-org.el
+++ helm-org.el
@@ -57,9 +57,9 @@ Note this have no effect in `helm-org-in-buffer-headings'."
 
 (defcustom helm-org-headings-actions
   '(("Go to heading" . helm-org-goto-marker)
-    ("Open in indirect buffer `C-RET'" . helm-org--open-heading-in-indirect-buffer)
-    ("Refile to this heading `C-w`''" . helm-org-heading-refile)
-    ("Insert link to this heading `C-l`''" . helm-org-insert-link-to-heading-at-marker))
+    ("Open in indirect buffer `C-c i'" . helm-org--open-heading-in-indirect-buffer)
+    ("Refile to this heading `C-c w`''" . helm-org-heading-refile)
+    ("Insert link to this heading `C-c l`''" . helm-org-insert-link-to-heading-at-marker))
   "Default actions alist for
   `helm-source-org-headings-for-files'."
   :group 'helm-org
@@ -100,19 +100,19 @@ Note this have no effect in `helm-org-in-buffer-headings'."
   (set-window-prev-buffers nil (append (cdr (window-prev-buffers))
                                        (car (window-prev-buffers)))))
 
-(defun helm-org--run-open-heading-in-indirect-buffer ()
+(defun helm-org-run-open-heading-in-indirect-buffer ()
   "Open selected Org heading in an indirect buffer."
   (interactive)
   (with-helm-alive-p
     (helm-exit-and-execute-action #'helm-org--open-heading-in-indirect-buffer)))
-(put 'helm-org--run-open-heading-in-indirect-buffer 'helm-only t)
+(put 'helm-org-run-open-heading-in-indirect-buffer 'helm-only t)
 
 (defvar helm-org-headings-map
   (let ((map (make-sparse-keymap)))
     (set-keymap-parent map helm-map)
-    (define-key map (kbd "<C-return>") 'helm-org--run-open-heading-in-indirect-buffer)
-    (define-key map (kbd "C-w") 'helm-org-heading-refile)
-    (define-key map (kbd "C-l") 'helm-org-insert-link-to-heading-at-marker)
+    (define-key map (kbd "<C-c i>") 'helm-org-run-open-heading-in-indirect-buffer)
+    (define-key map (kbd "C-c w")   'helm-org-run-heading-refile)
+    (define-key map (kbd "C-c l")   'helm-org-run-insert-link-to-heading-at-marker)
     map)
   "Keymap for `helm-source-org-headings-for-files'.")
 
@@ -218,6 +218,12 @@ Note this have no effect in `helm-org-in-buffer-headings'."
         (org-insert-link
          file-name (concat "file:" file-name "::*" heading-name))))))
 
+(defun helm-org-run-insert-link-to-heading-at-marker ()
+  (interactive)
+  (with-helm-alive-p
+    (helm-exit-and-execute-action
+     'helm-org-insert-link-to-heading-at-marker)))
+
 (defun helm-org-heading-refile (marker)
   (save-selected-window
     (when (eq major-mode 'org-agenda-mode)
@@ -229,6 +235,12 @@ Note this have no effect in `helm-org-in-buffer-headings'."
       (helm-org-goto-marker marker)
       (org-end-of-subtree t t)
       (org-paste-subtree (+ target-level 1)))))
+
+(defun helm-org-run-heading-refile ()
+  (interactive)
+  (with-helm-alive-p
+    (helm-exit-and-execute-action 'helm-org-heading-refile)))
+(put 'helm-org-run-heading-refile 'helm-only t)
 
 ;;;###autoload
 (defun helm-org-agenda-files-headings ()
