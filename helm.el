c8d2db8b89a2a94e3409c358afccd00e9a15e6a9
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-11-07 09:47:08 +0100

    Disable PA in action buffer for now.
    * helm.el (helm-show-action-buffer): Do it,
    need to fix it with follow enabled first.

--- helm.el
+++ helm.el
@@ -3648,12 +3648,8 @@ If action buffer is selected, back to the helm buffer."
           (helm-build-sync-source "Actions"
             :volatile t
             :nomark t
-            :persistent-action (lambda (_candidate)
-                                 (pcase (helm-get-selection helm-action-buffer)
-                                   ((pred byte-code-function-p) (ignore))
-                                   ((and fn) (helm-elisp--persistent-help
-                                              fn 'helm-describe-function))))
-            :persistent-help "Describe action"
+            :persistent-action (lambda (_candidate) (ignore))
+            :persistent-help "Do nothing"
             :keymap 'helm-map
             :candidates actions
             :mode-line '("Action(s)" "\\<helm-map>\\[helm-select-action]:BackToCands RET/f1/f2/fn:NthAct")
