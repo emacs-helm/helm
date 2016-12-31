aa0a15f2cfb3565f60b392aea868693f25a16d12
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-12-31 10:08:20 +0100

    Cleanup overlays when helm-buffer is empty.
    * helm-utils.el (helm-match-line-cleanup-maybe): New.
    Run it in helm-after-update-hook.

--- helm-utils.el
+++ helm-utils.el
@@ -666,6 +666,10 @@ Assume regexp is a pcre based regexp."
   (when helm--match-item-overlays
     (mapc 'delete-overlay helm--match-item-overlays)))
 
+(defun helm-match-line-cleanup-maybe ()
+  (when (helm-empty-buffer-p)
+    (helm-match-line-cleanup)))
+
 (defun helm-match-line-update ()
   (when helm-match-line-overlay
     (delete-overlay helm-match-line-overlay)
@@ -680,6 +684,7 @@ Assume regexp is a pcre based regexp."
 (defun helm-match-line-cleanup-pulse ()
   (run-with-timer 0.3 nil #'helm-match-line-cleanup))
 
+(add-hook 'helm-after-update-hook 'helm-match-line-cleanup-maybe)
 (add-hook 'helm-after-persistent-action-hook 'helm-persistent-autoresize-hook)
 (add-hook 'helm-cleanup-hook 'helm-match-line-cleanup)
 (add-hook 'helm-after-action-hook 'helm-match-line-cleanup-pulse)
