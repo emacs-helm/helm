1513ed821900db91f4da80343068a1cad244f974
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-01-12 07:58:48 +0100

    Add -A option by default for locate on GNU/Linux (#1317).
    * helm-locate.el (helm-locate-set-command): Should be safe now.

--- helm-locate.el
+++ helm-locate.el
@@ -222,7 +222,7 @@ See `helm-locate-with-db' and `helm-locate'."
   (unless helm-locate-command
     (setq helm-locate-command
           (cl-case system-type
-            (gnu/linux "locate %s -e --regex %s")
+            (gnu/linux "locate %s -e -A --regex %s")
             (berkeley-unix "locate %s %s")
             (windows-nt "es %s %s")
             (t "locate %s %s")))))
