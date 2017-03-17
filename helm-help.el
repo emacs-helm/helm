08228b0eca9dbd7650d682e7fef4dfbf0d1dac66
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-03-17 14:29:05 +0100

    Add help section for tramp in hff.
    * helm-help.el (helm-ff-help-message): Do it.

--- helm-help.el
+++ helm-help.el
@@ -518,6 +518,62 @@ However with a prefix arg it will apply `example' on each file:
 
 Of course the alias command should support this.
 
+*** Using Tramp with `helm-find-files' to read remote directories
+
+`helm-find-files' is working fine with tramp with however some limitations.
+
+- By default filenames are not highlighted when working on remote directories,
+this is controled by `helm-ff-tramp-not-fancy' variable, if you change this,
+expect helm becoming very slow unless your connection is super fast.
+
+- Grepping files is not very well supported when used incrementally, see above
+grep section.
+
+- Locate is not working on remote directories.
+
+**** Some reminders about Tramp syntax
+
+Not exhaustive, please read Tramp documentation.
+
+- Connect to host 192.168.0.4 as foo user:
+
+    /scp:192.168.0.4@foo:
+
+- Connect to host 192.168.0.4 as foo user with port 2222:
+
+    /scp:192.168.0.4@foo#2222:
+
+- Connect to host 192.168.0.4 as root using multihops syntax:
+
+    /ssh:192.168.0.4@foo|sudo:192.168.0.4:
+
+Note: you can also use `tramp-default-proxies-alist' when connecting often to
+some hosts.
+
+Prefer generally scp method unless using multihops (works only with ssh method)
+specially when copying large files.
+
+Note also that you have to hit once `C-j' on top of directory at first connection
+to complete your pattern in minibuffer.
+
+**** Completing host
+
+As soon as you enter the first \":\" after method e.g \"/scp:\" you will
+have some completion about previously used hosts or from your \"~/.ssh/config\"
+file, hitting `C-j' or `right' on a candidate will insert this host in minibuffer
+without addind the ending \":\".
+As soon the last \":\" Tramp will kick in and you should see the list of candidates
+a few seconds later.
+
+When your connection fails, be sure to delete your tramp connection before retrying
+with M-x `helm-delete-tramp-connection'.
+
+**** Editing local files as root
+
+Use the sudo method:
+
+    /sudo:host: or just /sudo::
+
 ** Commands
 \\<helm-find-files-map>
 \\[helm-ff-run-locate]\t\tRun Locate (C-u to specify locate db, M-n insert basename of candidate)
