91760bea40e3b19c2d843f63f0bfafb508772444
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-04-02 08:38:56 +0200

    Revert disabling menu-bar.
    * helm.el (helm-internal): Do it.

--- helm.el
+++ helm.el
@@ -2016,7 +2016,6 @@ ANY-KEYMAP ANY-DEFAULT ANY-HISTORY See `helm'."
         mode-line-in-non-selected-windows
         (input-method-verbose-flag helm-input-method-verbose-flag)
         (old--cua cua-mode)
-        (old--menu-bar menu-bar-mode)
         (helm--maybe-use-default-as-input
          (and (null any-input)
               (or helm--maybe-use-default-as-input ; it is let-bounded so use it.
@@ -2025,8 +2024,6 @@ ANY-KEYMAP ANY-DEFAULT ANY-HISTORY See `helm'."
     ;; cua-mode override local helm bindings.
     ;; disable this stupid thing if enabled.
     (and cua-mode (cua-mode -1))
-    ;; Prevent users clicking the f*%;!: thing.
-    (and menu-bar-mode (menu-bar-mode -1))
     (unwind-protect
          (condition-case-unless-debug _v
              (let ( ;; `helm--source-name' is non-`nil'
@@ -2075,7 +2072,6 @@ ANY-KEYMAP ANY-DEFAULT ANY-HISTORY See `helm'."
       (setq helm-sources nil)
       (setq helm--ignore-errors nil)
       (and old--cua (cua-mode 1))
-      (and old--menu-bar (menu-bar-mode 1))
       (helm-log-save-maybe))))
 
 
