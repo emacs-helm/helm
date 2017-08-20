c0cb8d58b0bf293316605611d708a249d559ba50
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-08-20 07:38:51 +0200

    Check parts of dabbrev in hash
    * helm-dabbrev.el (helm-dabbrev--cache-data): Do it.

--- helm-dabbrev.el
+++ helm-dabbrev.el
@@ -229,9 +229,13 @@ but the initial search for all candidates in buffer(s)."
 (defvar helm-dabbrev--hash (make-hash-table :test 'equal))
 
 (defun helm-dabbrev--cache-data (dabbrev)
-  (or (gethash dabbrev helm-dabbrev--hash)
-      (puthash dabbrev (helm-dabbrev--get-candidates dabbrev)
-               helm-dabbrev--hash)))
+  (unless (or (null dabbrev) (string= dabbrev ""))
+    (or (cl-loop for count downfrom (length dabbrev) to 1
+                 when (gethash (substring-no-properties dabbrev 0 count)
+                               helm-dabbrev--hash)
+                 return it)
+        (puthash dabbrev (helm-dabbrev--get-candidates dabbrev)
+                 helm-dabbrev--hash))))
 
 ;;;###autoload
 (defun helm-dabbrev ()
