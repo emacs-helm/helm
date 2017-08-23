e140406bf9250d18d5f7175eac7ad20f127c2c9b
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-08-23 11:52:47 +0200

    Allow resuming helm-occur with invisible props.
    * helm-regexp.el (helm-occur--invisible): Docstring.
    (helm-moccur-filter-one-by-one): Take an extra arg to notify the
    function is used outside helm session.
    (helm-moccur-mode--revert-buffer-function): Use it.
    (helm-occur): Set helm-occur--invisible locally.
    (helm-occur-from-isearch): Same.

--- helm-regexp.el
+++ helm-regexp.el
@@ -200,8 +200,10 @@ i.e Don't replace inside a word, regexp is surrounded with \\bregexp\\b."
 ;; Internal
 (defvar helm-multi-occur-buffer-list nil)
 (defvar helm-multi-occur-buffer-tick nil)
-(defvar helm-occur--invisible nil) ; Hide buffer name in results when
-                                   ; non-nil.
+(defvar helm-occur--invisible nil
+  "[INTERNAL] Hide buffer name in results when non-nil.
+Should be a local var to helm-buffer to allow resuming.")
+
 (defun helm-moccur-init ()
   "Create the initial helm multi occur buffer."
   (helm-init-candidates-in-buffer
@@ -405,7 +407,7 @@ Same as `helm-moccur-goto-line' but go in new frame."
                 (setq helm-multi-occur-buffer-tick new-tick-ls)
                 (message "Helm (m)occur Buffer have been udated")))))))))
 
-(defun helm-moccur-filter-one-by-one (candidate)
+(defun helm-moccur-filter-one-by-one (candidate &optional outside-helm)
   "`filter-one-by-one' function for `helm-source-moccur'."
   (require 'helm-grep)
   (let* ((split  (helm-grep-split-line candidate))
@@ -414,12 +416,16 @@ Same as `helm-moccur-goto-line' but go in new frame."
          (str    (nth 2 split)))
     (cons (concat (propertize
                    buf
-                   'invisible helm-occur--invisible
+                   'invisible (and (null outside-helm)
+                                   (with-helm-buffer
+                                     helm-occur--invisible))
                    'face 'helm-moccur-buffer
                    'help-echo (buffer-file-name
                                (get-buffer buf))
                    'buffer-name buf)
-                  (propertize ":" 'invisible helm-occur--invisible)
+                  (propertize ":" 'invisible (and (null outside-helm)
+                                                  (with-helm-buffer
+                                                    helm-occur--invisible)))
                   (propertize lineno 'face 'helm-grep-lineno)
                   ":"
                   (helm-grep-highlight-match str t))
@@ -609,7 +615,7 @@ Special commands:
                      do (with-current-buffer buffer
                           (insert
                             (propertize
-                             (car (helm-moccur-filter-one-by-one line))
+                             (car (helm-moccur-filter-one-by-one line t))
                              'helm-realvalue line)
                            "\n")))))
         (message "Reverting buffer done")))))
@@ -646,15 +652,16 @@ Special commands:
      'helm-multi-occur-buffer-tick
      (cl-loop for b in bufs
               collect (buffer-chars-modified-tick (get-buffer b)))))
-  (let ((helm-occur--invisible (null helm-occur-show-buffer-name)))
-    (helm :sources 'helm-source-occur
+  (helm-set-local-variable 'helm-occur--invisible
+                           (null helm-occur-show-buffer-name))
+  (helm :sources 'helm-source-occur
           :buffer "*helm occur*"
           :default (helm-aif (thing-at-point 'symbol) (regexp-quote it))
           :history 'helm-occur-history
           :preselect (and (memq 'helm-source-occur helm-sources-using-default-as-input)
                           (format "%s:%d:" (regexp-quote (buffer-name))
                                   (line-number-at-pos (point))))
-          :truncate-lines helm-moccur-truncate-lines)))
+          :truncate-lines helm-moccur-truncate-lines))
 
 ;;;###autoload
 (defun helm-occur-from-isearch ()
@@ -663,8 +670,7 @@ Special commands:
   (let ((input (if isearch-regexp
                    isearch-string
                  (regexp-quote isearch-string)))
-        (bufs (list (buffer-name (current-buffer))))
-        (helm-occur--invisible (null helm-occur-show-buffer-name)))
+        (bufs (list (buffer-name (current-buffer)))))
     (isearch-exit)
     (helm-occur-init-source)
     (helm-attrset 'moccur-buffers bufs helm-source-occur)
@@ -673,6 +679,8 @@ Special commands:
      'helm-multi-occur-buffer-tick
      (cl-loop for b in bufs
               collect (buffer-chars-modified-tick (get-buffer b))))
+    (helm-set-local-variable 'helm-occur--invisible
+                             (null helm-occur-show-buffer-name))
     (helm :sources 'helm-source-occur
           :buffer "*helm occur*"
           :history 'helm-occur-history
