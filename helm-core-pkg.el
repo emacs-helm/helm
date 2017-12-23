5b2057c7755f6ea20e1ea011c6fb992d12650161
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2017-12-23 09:01:38 +0100

    Update version in pkg files

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-core-pkg.el --- define helm-core for package.el
 
-(define-package "helm-core" "2.8.6"
+(define-package "helm-core" "2.8.7"
   "Development files for Helm"
   '((emacs "24.4")
     (async "1.9.2"))
