d5b9404176c41b16dffe522d45d08bdc7cb60e28
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-04-07 10:22:14 +0200

    Update version in pkg files

--- helm-pkg.el
+++ helm-pkg.el
@@ -1,11 +1,11 @@
 ;;; helm-pkg.el --- define helm for package.el
 
-(define-package "helm" "2.9.1"
+(define-package "helm" "2.9.2"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.2")
     (popup "0.5.3")
-    (helm-core "2.9.1"))
+    (helm-core "2.9.2"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
