4fa0814184f4ad9d4c3ad0c21f7f6ad97cc4471e
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-04-21 07:36:23 +0200

    Add a sort fn for recursives dirs in HFF
    * helm-files.el (helm-ff-sort-candidates-1): New, same as before but
    have an argument to choose input.
    (helm-ff-sort-candidates): Use it.
    (helm-find-files-recursive-dirs): Sort candidates.

--- helm-files.el
+++ helm-files.el
@@ -2564,12 +2564,12 @@ return FNAME prefixed with [?]."
       200
       (helm-score-candidate-for-pattern str pattern)))
 
-(defun helm-ff-sort-candidates (candidates _source)
+(defun helm-ff-sort-candidates-1 (candidates input)
   "Sort function for `helm-source-find-files'.
-Return candidates prefixed with basename of `helm-input' first."
-  (if (or (and (file-directory-p helm-input)
-               (string-match "/\\'" helm-input))
-          (string-match "\\`\\$" helm-input)
+Return candidates prefixed with basename of INPUT first."
+  (if (or (and (file-directory-p input)
+               (string-match "/\\'" input))
+          (string-match "\\`\\$" input)
           (null candidates))
       candidates
       (let* ((c1        (car candidates))
@@ -2581,7 +2581,7 @@ Return candidates prefixed with basename of `helm-input' first."
                         (lambda (s1 s2)
                             (let* ((score (lambda (str)
                                             (helm-ff-score-candidate-for-pattern
-                                             str (helm-basename helm-input))))
+                                             str (helm-basename input))))
                                    (bn1 (helm-basename (if (consp s1) (cdr s1) s1)))
                                    (bn2 (helm-basename (if (consp s2) (cdr s2) s2)))
                                    (sc1 (or (gethash bn1 memo-src)
@@ -2593,6 +2593,11 @@ Return candidates prefixed with basename of `helm-input' first."
                                         (string-width bn2)))
                                     ((> sc1 sc2))))))))
         (if cand1 (cons cand1 all) all))))
+
+(defun helm-ff-sort-candidates (candidates _source)
+  "Sort function for `helm-source-find-files'.
+Return candidates prefixed with basename of `helm-input' first."
+  (helm-ff-sort-candidates-1 candidates helm-input))
 
 (defun helm-ff-boring-file-p (file)
   ;; Prevent user doing silly thing like
@@ -2968,7 +2973,9 @@ If a prefix arg is given or `helm-follow-mode' is on open file."
                                    (string-match-p ,(regexp-quote input)
                                                    (helm-basename c)))
                          collect (propertize c 'face 'helm-ff-dirs)))
-              helm-w32-pathname-transformer)
+              helm-w32-pathname-transformer
+              (lambda (candidates)
+                (helm-ff-sort-candidates-1 candidates ,input)))
             :persistent-action 'ignore
             :action (lambda (c)
                       (helm-set-pattern
