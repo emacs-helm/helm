fe3dc6657386d181c38bc1707d5b886d0502abab
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-04-24 07:00:21 +0200

    Use async 1.9.3 as dependency

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -3,7 +3,7 @@
 (define-package "helm-core" "2.9.2"
   "Development files for Helm"
   '((emacs "24.4")
-    (async "1.9.2"))
+    (async "1.9.3"))
   :url "https://emacs-helm.github.io/helm/")
 
 ;; Local Variables:
--- helm-pkg.el
+++ helm-pkg.el
@@ -3,7 +3,7 @@
 (define-package "helm" "2.9.2"
   "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
-    (async "1.9.2")
+    (async "1.9.3")
     (popup "0.5.3")
     (helm-core "2.9.2"))
   :url "https://emacs-helm.github.io/helm/")
