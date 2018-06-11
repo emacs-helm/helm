6f66b29e746a28749b650972ea7a57616e7c573d
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-06-11 10:36:31 +0200

    Fix typo

--- helm-help.el
+++ helm-help.el
@@ -826,7 +826,7 @@ A good compromise is to trash your files
 when using asynchronous method (see [[Trashing files][Trashing files]]).
 
 Note that emacs is always making a backup of your files when
-deleting, media though are definitely deleted with no backup.
+deleting, medias though are definitely deleted with no backup.
 
 When choosing synchronous delete, you can allow recursive
 deletion of directories with `helm-ff-allow-recursive-deletes'.
