57125864d1a59bd77dbaa371005c7bdbad13b448
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-06-15 12:14:29 +0200

    Kill buffer before mode-line notification
    and prevent asking y/n in widget.
    
    * helm-files.el (helm-delete-marked-files-async): Do it.

--- helm-files.el
+++ helm-files.el
@@ -3739,6 +3739,11 @@ always deleted with no warnings."
                        (fit-window-to-buffer (get-buffer-window
                                               "*helm delete files*"))
                        (delete-file helm-ff-delete-log-file))
+                     (when buffers
+                       (dolist (buf buffers)
+                         (let ((last-nonmenu-event t))
+                           (when (y-or-n-p (format "Kill buffer %s, too? " buf))
+                             (kill-buffer buf)))))
                      (run-with-timer
                       0.1 nil
                       (lambda ()
@@ -3746,11 +3751,7 @@ always deleted with no warnings."
                          "%s (%s/%s) file(s) async done"
                          'helm-delete-async-message
                          (if trash "Trashing" "Deleting")
-                         result (length files))
-                        (when buffers
-                          (dolist (buf buffers)
-                            (when (y-or-n-p (format "Kill buffer %s, too? " buf))
-                              (kill-buffer buf))))))))
+                         result (length files))))))
          ;; Workaround emacs-26 bug with tramp see
          ;; https://github.com/jwiegley/emacs-async/issues/80.
          (async-quiet-switch "-q"))
