071ff9576433b68d1dcf1226522df66f51715c2d
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2018-12-11 09:14:58 +0100

    Fix a bug with prefixed fnames in hff
    where pattern is matching a file but is at the same time equal to
    another existing file, e.g. ack and ack-grep in /usr/bin.
    
    * helm-files.el (helm-find-files-get-candidates): Check earlier (here)
    if pattern is an existing file.
    (helm-ff-sort-candidates-1): move test of "pattern is an existing file"
    to helm-find-files-get-candidates as is is too late to do it here for
    remote files.

--- helm-files.el
+++ helm-files.el
@@ -2392,6 +2392,22 @@ purpose."
           ((and dir-p helm-ff-auto-update-flag)
            (helm-ff-directory-files path))
           (t (append (unless (or require-match
+                                 ;; Check here if path is an existing
+                                 ;; file before adding it to
+                                 ;; candidates, it was previously done
+                                 ;; in the sort function but this
+                                 ;; create a bug with remote files
+                                 ;; when path is at the same time a
+                                 ;; pattern matching a candidate and a
+                                 ;; real candidate e.g. ack and
+                                 ;; ack-grep in /usr/bin. This is due
+                                 ;; presumably to a latency more
+                                 ;; important with remote files which
+                                 ;; lead to a confusion with the
+                                 ;; pattern matching one candidate and
+                                 ;; the real candidate which is same
+                                 ;; as pattern.
+                                 (file-exists-p path)
                                  ;; When `helm-ff-auto-update-flag' has been
                                  ;; disabled, whe don't want PATH to be added on top
                                  ;; if it is a directory.
@@ -2763,12 +2779,8 @@ Return candidates prefixed with basename of INPUT first."
           (string-match "\\`\\$" input)
           (null candidates))
       candidates
-      (let* ((c1        (car candidates))
-             (cand1real (if (consp c1) (cdr c1) c1))
-             (cand1     (unless (file-exists-p cand1real) c1))
-             (rest-cand (if cand1 (cdr candidates) candidates))
-             (memo-src  (make-hash-table :test 'equal))
-             (all (sort rest-cand
+      (let* ((memo-src  (make-hash-table :test 'equal))
+             (all (sort candidates
                         (lambda (s1 s2)
                             (let* ((score (lambda (str)
                                             (helm-ff-score-candidate-for-pattern
@@ -2783,7 +2795,7 @@ Return candidates prefixed with basename of INPUT first."
                                      (< (string-width bn1)
                                         (string-width bn2)))
                                     ((> sc1 sc2))))))))
-        (if cand1 (cons cand1 all) all))))
+        all)))
 
 (defun helm-ff-sort-candidates (candidates _source)
   "Sort function for `helm-source-find-files'.
