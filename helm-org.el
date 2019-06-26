c88543e3d51ff836ff30fd41c1bbe3190aa842c4
Jonathan Gregory <jgrg@autistici.org>
2019-06-25 21:13:00 -0300

    Remove unneeded search backward function
    * helm-org.el (helm-org-goto-marker): Do it.

--- helm-org.el
+++ helm-org.el
@@ -188,12 +188,16 @@ when called from the `helm-org-agenda-files-headings' command."
    (keymap :initform 'helm-org-headings-map)
    (group :initform 'helm-org)))
 
+
+;; Actions
+;;
+;;
+
 (defun helm-org-goto-marker (marker)
   "Go to MARKER showing the entry's context, body and subheadings."
   (switch-to-buffer (marker-buffer marker))
   (goto-char (marker-position marker))
   (org-show-context)
-  (re-search-backward "^\\*+ " nil t)
   (org-show-entry)
   (org-show-children))
 
@@ -214,65 +218,6 @@ when called from the `helm-org-agenda-files-headings' command."
   (with-helm-alive-p
     (helm-exit-and-execute-action #'helm-org--open-heading-in-indirect-buffer)))
 (put 'helm-org-run-open-heading-in-indirect-buffer 'helm-only t)
-
-(defun helm-org--get-candidates-in-file (filename &optional fontify nofname parents)
-  (with-current-buffer (pcase filename
-                         ((pred bufferp) filename)
-                         ((pred stringp) (find-file-noselect filename t)))
-    (let ((match-fn (if fontify
-                        #'match-string
-                      #'match-string-no-properties))
-          (search-fn (lambda ()
-                       (re-search-forward
-                        org-complex-heading-regexp nil t)))
-          (file (unless (or (bufferp filename) nofname)
-                  (concat (helm-basename filename) ":"))))
-      (when parents
-        (add-function :around (var search-fn)
-                      (lambda (old-fn &rest args)
-                                (when (org-up-heading-safe)
-                                  (apply old-fn args)))))
-      (save-excursion
-        (save-restriction
-          (unless (and (bufferp filename)
-                       (buffer-base-buffer filename))
-            ;; Only widen direct buffers, not indirect ones.
-            (widen))
-          (unless parents (goto-char (point-min)))
-          ;; clear cache for new version of org-get-outline-path
-          (and (boundp 'org-outline-path-cache)
-               (setq org-outline-path-cache nil))
-          (cl-loop with width = (window-width (helm-window))
-                   while (funcall search-fn)
-                   for beg = (point-at-bol)
-                   for end = (point-at-eol)
-                   when (and fontify
-                             (null (text-property-any
-                                    beg end 'fontified t)))
-                   do (jit-lock-fontify-now beg end)
-                   for level = (length (match-string-no-properties 1))
-                   for heading = (funcall match-fn 4)
-                   if (and (>= level helm-org-headings-min-depth)
-                           (<= level helm-org-headings-max-depth))
-                   collect `(,(propertize
-                               (if helm-org-format-outline-path
-                                   (org-format-outline-path
-                                    ;; org-get-outline-path changed in signature and behaviour since org's
-                                    ;; commit 105a4466971. Let's fall-back to the new version in case
-                                    ;; of wrong-number-of-arguments error.
-                                    (condition-case nil
-                                        (append (apply #'org-get-outline-path
-                                                       (unless parents
-                                                         (list t level heading)))
-                                                (list heading))
-                                      (wrong-number-of-arguments
-                                       (org-get-outline-path t t)))
-                                    width file)
-                                   (if file
-                                       (concat file (funcall match-fn  0))
-                                       (funcall match-fn  0)))
-                               'helm-real-display heading)
-                              . ,(point-marker))))))))
 
 (defun helm-org-insert-link-to-heading-at-marker (marker)
   (with-current-buffer (marker-buffer marker)
