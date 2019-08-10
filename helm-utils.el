dde90de5c5408e22503f23ed0310f3277f4f6014
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2019-08-10 20:10:46 +0200

    Remove helm-org.el
    * helm-utils.el (outline-show-subtree): Declare.
    (org-reveal):                           Declare.
    (org-directory):                        Declare.

--- helm-utils.el
+++ helm-utils.el
@@ -25,6 +25,9 @@
 (declare-function helm-find-files-1 "helm-files.el" (fname &optional preselect))
 (declare-function popup-tip "ext:popup")
 (declare-function markdown-show-subtree "outline.el")
+(declare-function outline-show-subtree "outline.el")
+(declare-function org-reveal "org.el")
+(defvar org-directory)
 (defvar winner-boring-buffers)
 (defvar helm-show-completion-overlay)
 
