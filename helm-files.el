ed73f602a34f531d8ce81bd1897fc52a4d0c220f
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2019-09-28 16:50:16 +0200

    Handle dups in marked candidates in esh cmd on file
    * helm-files.el (helm-find-files-eshell-command-on-file-1): Do it.

--- helm-files.el
+++ helm-files.el
@@ -1054,7 +1054,8 @@ working."
                                   default-directory))
            (command (with-helm-display-marked-candidates
                       helm-marked-buffer-name
-                      (mapcar 'helm-basename cand-list)
+                      (helm-ff--count-and-collect-dups
+                       (mapcar 'helm-basename cand-list))
                       (helm-comp-read
                        "Command: "
                        (cl-loop for (a c) in (eshell-read-aliases-list)
