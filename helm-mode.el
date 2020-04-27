6b908f62df6762120c05af98d67b19b11349478a
Iqbal Ansari <iqbalansari02@yahoo.com>
2020-04-27 11:17:11 +0530

    Use instead `equal` instead of `string=` to compare display properties
    This avoids the error that might occur if the candidates have image set display property

--- helm-mode.el
+++ helm-mode.el
@@ -478,9 +478,7 @@ If COLLECTION is an `obarray', a TEST should be needed. See `obarray'."
 (defun helm-comp-read--move-to-first-real-candidate ()
   (helm-aif (helm-get-selection nil 'withprop)
       ;; Avoid error with candidates with an image as display (#2296).
-      (when (or (and (stringp it)
-                     (string= (get-text-property 0 'display it) "[?]"))
-                (not (stringp it)))
+      (when (equal (get-text-property 0 'display it) "[?]")
         (helm-next-line))))
 
 (defun helm-cr-default (default cands)
