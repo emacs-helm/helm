3c27524408ab26f0ec91fe1d7e025afbc7921571
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2020-07-28 14:22:56 +0200

    Remove call to eieio-declare-slots

--- helm-types.el
+++ helm-types.el
@@ -36,9 +36,6 @@
 (defclass helm-type-file (helm-source) ()
   "A class to define helm type file.")
 
-;; Emacs-27+ only. FIXME Should be fixed in helm-source instead.
-(when (fboundp 'eieio-declare-slots)
-  (eieio-declare-slots 'action))
 (defmethod helm-source-get-action-from-type ((object helm-type-file))
   (slot-value object 'action))
 
