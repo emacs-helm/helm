1f32ad2793222985cb8a3bf9a346f4448618224d
Thierry Volpiatto <thievol@posteo.net>
2020-09-01 15:04:39 +0200

    Update *pkg files

--- helm-core-pkg.el
+++ helm-core-pkg.el
@@ -1,6 +1,6 @@
 ;;; helm-core-pkg.el --- define helm-core for package.el
 
-(define-package "helm-core" "3.6.2"
+(define-package "helm-core" "3.6.5"
   "Development files for Helm"
   '((emacs "25.1")
     (async "1.9.4"))
