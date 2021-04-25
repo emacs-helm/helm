e15de357d682dabd52b7bbe46f1726156bd68c21
Thierry Volpiatto <thievol@posteo.net>
2021-04-25 13:57:41 +0200

    Fix helm commands called from minibuffer fail to exit
    `selected-frame' is returning the wrong frame on emacs-28 when
    helm is called from and active minibuffer
    e.g. completion-at-point from M-:, so use with-helm-window at the
    risk of having an error if window have been already deleted (is
    it possible?).

--- helm.el
+++ helm.el
@@ -3910,7 +3910,12 @@ WARNING: Do not use this mode yourself, it is internal to Helm."
 (defun helm-cleanup ()
   "Clean up the mess when Helm exit or quit."
   (helm-log "start cleanup")
-  (with-current-buffer helm-buffer
+  ;; `selected-frame' is returning the wrong frame on emacs-28 when
+  ;; helm is called from and active minibuffer
+  ;; e.g. completion-at-point from M-:, so use with-helm-window at the
+  ;; risk of having an error if window have been already deleted (is
+  ;; it possible?).
+  (with-helm-window
     (let ((frame (selected-frame)))
       (setq cursor-type t)
       ;; Ensure restoring default-value of mode-line to allow user
