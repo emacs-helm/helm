53f98964afd95a5aba60d7ce82d7bb4b693e46a3
Thierry Volpiatto <thievol@posteo.net>
2021-04-25 09:10:02 +0200

    Add an exit-minibuffer function
    This is the same as old exit-minibuffer function used in emacs-27.2 and prior.
    We use this as the one in emacs-28 is sucking most of the time.

--- helm.el
+++ helm.el
@@ -2594,7 +2594,7 @@ of current source only."
          ,@body)
      (quit (setq quit-flag t)
            (setq helm-quit t)
-           (exit-minibuffer)
+           (helm--exit-minibuffer-internal)
            (keyboard-quit)
            ;; See comment about this in `with-local-quit'.
            (eval '(ignore nil)))))
@@ -5984,7 +5984,7 @@ message 'no match'."
     (setq helm-current-prefix-arg current-prefix-arg))
   (setq helm-exit-status 0)
   (helm-log-run-hook 'helm-exit-minibuffer-hook)
-  (exit-minibuffer))
+  (helm--exit-minibuffer-internal))
 
 (defun helm-keyboard-quit ()
   "Quit minibuffer in helm.
