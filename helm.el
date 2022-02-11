f75e1d82bec733d077983e56c2bf412df9b4d402
Thierry Volpiatto <thievol@posteo.net>
2022-02-11 21:40:37 +0100

    Add a Commentary header in helm.el

--- helm.el
+++ helm.el
@@ -28,6 +28,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; No commentary.
+
 ;;; Code:
 
 (require 'cl-lib)
