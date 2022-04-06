06772626648242fe308b9118518af36a29c9f2ed
Thierry Volpiatto <thievol@posteo.net>
2022-04-06 07:58:36 +0200

    Fix bug-reference-bug-regexp to shutup BC in emacs-28+

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((bug-reference-bug-regexp . "\\([Ii]ssue ?#?\\|[Bb]ug ?#?\\|[Pp]atch ?#\\|RFE ?#\\|PR [a-z+-]+/\\)\\([0-9]+\\(?:#[0-9]+\\)?\\)")
+((nil . ((bug-reference-bug-regexp . "\\(\\b\\([Ii]ssue ?#?\\|[Bb]ug ?#?\\|[Pp]atch ?#\\|RFE ?#\\|PR [a-z+-]+/\\)\\([0-9]+\\(?:#[0-9]+\\)?\\)\\)")
          (bug-reference-url-format . "https://github.com/emacs-helm/helm/issues/%s")
          (byte-compile-warnings . (not obsolete not docstrings))))
  (emacs-lisp-mode . ((mode . bug-reference-prog)
