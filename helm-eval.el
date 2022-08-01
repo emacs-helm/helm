56c2230d03c27ee1c97d633ab6bbe95abaf76ccb
Thierry Volpiatto <thievol@posteo.net>
2022-08-01 18:34:54 +0200

    Revert partially 1b1f1ed9 for helm-eval as the feature IS working
    in emacs-27. Emacs-28+ is more restrictive and works only if
    minibuffer-follows-selected-frame is indeed deactivated.
    See window-deletable-p which should return 'frame in such case.

--- helm-eval.el
+++ helm-eval.el
@@ -180,12 +180,7 @@ Should take one arg: the string to display."
 (defun helm-eval-expression (arg)
   "Preconfigured `helm' for `helm-source-evaluation-result'."
   (interactive "P")
-  (let ((helm-elisp-help-function #'helm-elisp-show-doc-modeline)
-        (helm-show-completion-display-function
-         ;; Nested completion in a frame works only in emacs-28+ when
-         ;; minibuffer-follows-selected-frame is available.
-         (and (boundp 'minibuffer-follows-selected-frame)
-              (default-value 'helm-show-completion-display-function))))
+  (let ((helm-elisp-help-function #'helm-elisp-show-doc-modeline))
     (helm :sources (helm-build-evaluation-result-source)
           :input (when arg (thing-at-point 'sexp))
           :buffer "*helm eval*"
