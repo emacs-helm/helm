299d0e887175f1c9c364201d604f4141899f3b0f
Thierry Volpiatto <thievol@posteo.net>
2023-01-02 14:30:09 +0100

    Commentary only

--- helm-core.el
+++ helm-core.el
@@ -23,6 +23,8 @@
 ;;; Commentary:
 
 ;; Contains the main code for Helm.
+;; As a package helm-core provides the files helm-core.el, helm-lib.el,
+;; helm-source.el and helm-multi-match.el.
 
 ;;; Code:
 
