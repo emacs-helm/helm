88e4d1aed391c5e3e88dcdc8ebd02dc585c092b5
Thierry Volpiatto <thievol@posteo.net>
2023-08-02 16:49:09 +0200

    Fix #2610 by flattening candidates list
    This fix as well #2607 which was fixed for Emacs-28 but reappears in
    Emacs-29.1 for some reasons.

--- helm-mode.el
+++ helm-mode.el
@@ -1854,15 +1854,18 @@ Return a list of cons cells of the form (disp . real)."
 
 If both AFUN and AFIX are provided only AFIX is used.
 When FILE-COMP-P is provided only filter out dot files."
+  ;; Filter out dot files in file completion. Normally COMPS should be a list of
+  ;; string but in some cases it is given as a list of strings containing a list
+  ;; of string e.g. ("a" "b" "c" ("d" "e" "f")) ; This happen in rgrep
+  ;; (bug#2607) and highlight-* fns (bug #2610), so ensure the list is flattened to
+  ;; avoid e.g. wrong-type argument: stringp '("d" "e" "f")
+  ;; FIXME: If this create a new bug with completion-in-region, flatten COMPS
+  ;; directly in the caller i.e. helm-completing-read-default-1.
+  (setq comps (helm-fast-remove-dups
+               (helm-flatten-list comps)
+               :test 'equal))
   (if file-comp-p
-      ;; Filter out dot files in file completion. Normally COMPS should be a
-      ;; list of string but in some cases it is given as a list of strings
-      ;; containing a list of string e.g. ("a" "b" "c" ("d" "e" "f")) ; This
-      ;; happen in rgrep (bug #2607), so ensure the list is flattened to avoid
-      ;; e.g. wrong-type argument '("d" "e" "f")
-      (cl-loop for f in (helm-fast-remove-dups
-                         (helm-flatten-list comps)
-                         :test 'equal)
+      (cl-loop for f in comps
                unless (string-match "\\`\\.\\{1,2\\}/\\'" f)
                collect f)
     (cond (afix (helm--symbol-completion-table-affixation comps))
