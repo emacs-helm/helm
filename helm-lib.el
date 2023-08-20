db3f9e143ce6a208175511b412a67edc8c8b89b4
Thierry Volpiatto <thievol@posteo.net>
2023-08-20 12:10:19 +0200

    Fix helm-describe-class

--- helm-lib.el
+++ helm-lib.el
@@ -1191,8 +1191,10 @@ differently depending of answer:
   "Display documentation of Eieio CLASS, a symbol or a string."
   (advice-add 'cl--print-table :override #'helm-source--cl--print-table '((depth . 100)))
   (unwind-protect
-       (let ((helm-describe-function-function 'describe-function))
-         (helm-describe-function class))
+       (if (fboundp 'cl-describe-type)
+           (cl-describe-type (helm-symbolify class))
+         (let ((helm-describe-function-function 'describe-function))
+           (helm-describe-function (helm-symbolify class))))
     (advice-remove 'cl--print-table #'helm-source--cl--print-table)))
 
 (defun helm-describe-function (func)
