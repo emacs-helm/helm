b04272a53e5b2db1fc7724d4ba84e51fd20e9c4f
Thierry Volpiatto <thievol@posteo.net>
2023-12-09 08:09:46 +0100

    Bump to 3.9.7

--- helm.el
+++ helm.el
@@ -11,9 +11,9 @@
 ;;                         Thierry Volpiatto
 
 ;; Author: Thierry Volpiatto <thievol@posteo.net>
-;; Version: 3.9.6
+;; Version: 3.9.7
 ;; URL: https://emacs-helm.github.io/helm/
-;; Package-Requires: ((helm-core "3.9.6") (wfnames "1.1") (popup "0.5.3"))
+;; Package-Requires: ((helm-core "3.9.7") (wfnames "1.1") (popup "0.5.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
