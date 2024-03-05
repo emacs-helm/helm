fd5bffaa655e641ea47eacef1fa2f64b3846eee3
Thierry Volpiatto <thievol@posteo.net>
2024-03-05 11:41:28 +0100

    Fix def-edebug-elem-spec not available in Emacs<28 (#2641)

--- helm-lib.el
+++ helm-lib.el
@@ -565,9 +565,6 @@ is usable in next condition."
         (t `(helm-aif ,(car conditions)
                 (helm-aand ,@(cdr conditions))))))
 
-(def-edebug-elem-spec 'helm-acase-GUARD
-  '(&or (symbolp form) sexp))
-
 (defmacro helm-acase (expr &rest clauses)
   "A simple anaphoric case implementation.
 
@@ -584,7 +581,7 @@ EXPR is bound to a temporary variable called `it' which is
 usable in CLAUSES to refer to EXPR.
 
 \(fn EXPR (KEYLIST BODY...)...)"
-  (declare (indent 1) (debug (form &rest (helm-acase-GUARD body))))
+  (declare (indent 1) (debug (form &rest ([&or (symbolp form) sexp] body))))
   (unless (null clauses)
     (let* ((clause1 (car clauses))
            (key     (car clause1))
