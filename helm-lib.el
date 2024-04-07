dcce3aa55e36a68f8c536a9b7a6c11c9fc94e950
Thierry Volpiatto <thievol@posteo.net>
2024-04-07 21:34:10 +0200

    Eval and compile without-remote-files def (#2661)

--- helm-lib.el
+++ helm-lib.el
@@ -411,13 +411,14 @@ This is done recursively."
 	      (setf (aref vec i) (+ k ?\M-\C-@ -128))))))
       vec))))
 
-(unless (fboundp 'without-remote-files)
-  (defmacro without-remote-files (&rest body)
-  "Deactivate remote file names temporarily and run BODY."
-  (declare (indent 0) (debug ((form body) body)))
-  `(let ((file-name-handler-alist (copy-tree file-name-handler-alist))
-         tramp-mode)
-     (tramp-unload-file-name-handlers) ,@body)))
+(eval-and-compile
+  (unless (fboundp 'without-remote-files)
+    (defmacro without-remote-files (&rest body)
+      "Deactivate remote file names temporarily and run BODY."
+      (declare (indent 0) (debug ((form body) body)))
+      `(let ((file-name-handler-alist (copy-tree file-name-handler-alist))
+             tramp-mode)
+         (tramp-unload-file-name-handlers) ,@body))))
 
 ;;; Macros helper.
 ;;
