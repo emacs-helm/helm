a6d36ebec7a9be2ba2613e00bedddc3d0ea43778
Thierry Volpiatto <thievol@posteo.net>
2024-05-21 16:01:24 +0200

    truncate-string-ellipsis is not available in 27.1 (#2673)
    The function is not available. So inline directly the definition to
    set helm-buffers-end-truncated-string.

--- helm-buffers.el
+++ helm-buffers.el
@@ -18,7 +18,6 @@
 ;;; Code:
 
 (require 'cl-lib)
-(require 'mule-util)
 (require 'helm)
 (require 'helm-types)
 (require 'helm-utils)
@@ -102,7 +101,10 @@ When adding a source here it is up to you to ensure the library
 of this source is accessible and properly loaded."
   :type '(repeat (choice symbol)))
 
-(defcustom helm-buffers-end-truncated-string (truncate-string-ellipsis)
+(defcustom helm-buffers-end-truncated-string
+  ;; `truncate-string-ellipsis', the function is not available in 27.1
+  ;; See issue#2673. 
+  (if (char-displayable-p ?…) "…" "...")
   "The string to display at end of truncated buffer names."
   :type 'string)
 
