4a4b2a44948a7cac8c52e260e5c835249ce04224
Thierry Volpiatto <thievol@posteo.net>
2024-07-26 18:00:32 +0200

    Docstring only

--- helm-core.el
+++ helm-core.el
@@ -351,7 +351,7 @@ on exit.
 For any other key pressed, run their assigned command as defined
 in MAP and then exit the loop running EXIT-FN, if specified.
 
-If DELAY an integer is specified exit after DELAY seconds.
+If DELAY is specified the command expires after DELAY seconds.
 
 NOTE: SUBKEY and OTHER-SUBKEYS bindings support only char syntax
 and vectors, so don't use strings to define them.  While defining
