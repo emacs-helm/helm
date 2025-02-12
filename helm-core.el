98c0d4e62ee3690da97323a4215bddda7f51e4fa
Thierry Volpiatto <thievol@posteo.net>
2025-02-12 15:09:52 +0100

    Bump to 4.0.1

--- helm-core.el
+++ helm-core.el
@@ -4,8 +4,8 @@
 
 ;; Author: Thierry Volpiatto <thievol@posteo.net>
 ;; URL: https://emacs-helm.github.io/helm/
-;; Version: 4.0
-;; Package-Requires: ((emacs "25.1") (async "1.9.8"))
+;; Version: 4.0.1
+;; Package-Requires: ((emacs "25.1") (async "1.9.9"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
--- helm.el
+++ helm.el
@@ -11,9 +11,9 @@
 ;;                         Thierry Volpiatto
 
 ;; Author: Thierry Volpiatto <thievol@posteo.net>
-;; Version: 4.0
+;; Version: 4.0.1
 ;; URL: https://emacs-helm.github.io/helm/
-;; Package-Requires: ((helm-core "4.0") (wfnames "1.2"))
+;; Package-Requires: ((helm-core "4.0.1") (wfnames "1.2"))
 ;; Keywords: helm, convenience, files, buffers, grep, completion, lisp, matching, tools, help
 
 ;; This program is free software; you can redistribute it and/or modify
