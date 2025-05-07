91884032484d2321ed419abf4f2d4cffce895d24
Thierry Volpiatto <thievol@posteo.net>
2025-05-07 06:16:59 +0200

    Ensure minibuffer focus is restored
    in any cases.

--- helm-files.el
+++ helm-files.el
@@ -5667,9 +5667,9 @@ When no suitable place to drop is found ask to drop to
                         ((car windows)
                          (with-selected-window it default-directory)))))
     (if (memq helm-ff-drag-mouse-1-default-action '(copy rsync))
-        (progn
-          (helm-find-files-do-action
-           helm-ff-drag-mouse-1-default-action target)
+        (unwind-protect
+             (helm-find-files-do-action
+              helm-ff-drag-mouse-1-default-action target)
           ;; Retrieve focus after dropping
           (with-selected-frame helm-initial-frame
             (select-window (minibuffer-window))))
