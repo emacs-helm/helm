82d3e97ecdfb2f2e581ecc1299ef49c5431bea34
Thierry Volpiatto <thievol@posteo.net>
2025-09-23 05:22:46 +0200

    Add extra fontification for helm-acase

--- helm-core.el
+++ helm-core.el
@@ -34,6 +34,10 @@
 (require 'helm-multi-match)
 (require 'helm-source)
 
+(defun helm-acase-extra-fontifications ()
+  (font-lock-add-keywords nil '(("\\<\\(guard\\|dst\\)\\>\\*" . font-lock-type-face))))
+(add-hook 'emacs-lisp-mode-hook #'helm-acase-extra-fontifications)
+
 ;; Setup completion styles for helm-mode
 (helm--setup-completion-styles-alist)
 
